33984418e9fbe6370d862173ff1767c434d8fa95
Eduardo Liron <eduardoliron@gmail.com>
2024-01-18 11:12:24 -0300

    fix(UserStats): add key prop to list elements as required

--- src/components/UserStats/UserStats.tsx
+++ src/components/UserStats/UserStats.tsx
@@ -8,8 +8,8 @@ if(displayPhases.includes('result')) displayPhases.splice(displayPhases.indexOf(
 const UserStats = () => {
   return (
     <Grid container spacing={1} py={1}>
-      {displayPhases.map(phase => (
-        <Grid item xs={6}>
+      {displayPhases.map((phase, i) => (
+        <Grid item xs={6} key={i}>
           <PhaseButton variant={phase} displayNumber={3} />
         </Grid>
       ))}
