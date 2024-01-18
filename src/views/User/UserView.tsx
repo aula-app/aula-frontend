c1ed925dc3b6ee250a01f5e9865312299c36e46c
Eduardo Liron <eduardoliron@gmail.com>
2024-01-18 17:30:09 -0300

    fix(UserView.tsx): fix type error regarding unused variables in useEffect calls

--- src/views/User/UserView.tsx
+++ src/views/User/UserView.tsx
@@ -8,7 +8,7 @@ import { useEffect, useState } from 'react';
  * url: /user
  */
 const UserView = () => {
-  const [data, setData] = useState([] as any[]);
+  const [, setData] = useState([] as any[]);
   const jwt_token = localStorageGet('token');
 
   useEffect(() => {
