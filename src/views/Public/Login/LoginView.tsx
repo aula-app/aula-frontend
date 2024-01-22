bc618c5c7524de6d4817c1f0992a8758b4a86ba7
Eduardo Liron <eduardoliron@gmail.com>
2024-01-22 15:48:42 -0300

    wip(ConfirmEmailView): update paths to @/ paths"

---
+++ src/views/Public/Login/LoginView.tsx
@@ -0,0 +1,152 @@
+import { SyntheticEvent, useCallback, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  Button,
+  Grid,
+  TextField,
+  InputAdornment,
+  Stack,
+  Typography,
+} from '@mui/material';
+import { useAppStore } from '@/store';
+import { localStorageSet } from '@/utils/localStorage';
+import { AppButton, AppLink, AppIconButton, AppAlert, AppForm } from '@/components';
+import { useAppForm, SHARED_CONTROL_PROPS, eventPreventDefault } from '@/utils/form';
+
+const VALIDATE_FORM_LOGIN = {
+  username: {
+    presence: true,
+  },
+  password: {
+    presence: true,
+    length: {
+      minimum: 4,
+      maximum: 32,
+      message: 'must be between 4 and 32 characters',
+    },
+  },
+};
+
+interface FormStateValues {
+  username: string;
+  password: string;
+}
+
+/**
+ * Renders "Login" view for Login flow
+ * url: /login/email
+ */
+const LoginView = () => {
+  const navigate = useNavigate();
+  const [, dispatch] = useAppStore();
+  const [formState, , onFieldChange, fieldGetError, fieldHasError] = useAppForm({
+    validationSchema: VALIDATE_FORM_LOGIN,
+    initialValues: { username: '', password: '' } as FormStateValues,
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState<string>();
+  const values = formState.values as FormStateValues; // Typed alias to formState.values as the "Source of Truth"
+
+  const handleShowPasswordClick = useCallback(() => {
+    setShowPassword((oldValue) => !oldValue);
+  }, []);
+
+  const handleFormSubmit = useCallback(
+    async (event: SyntheticEvent) => {
+      event.preventDefault();
+      try {
+        const username = values['username'];
+        const password = values['password'];
+        const data = await (
+          await fetch(import.meta.env.VITE_APP_API_URL + '/api/controllers/login.php', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username: username, password: password }),
+          })
+        ).json();
+
+        const result = data.success; // await api.auth.loginWithEmail(values);
+        if (result) {
+          localStorageSet('token', data['JWT']);
+        }
+
+        if (!result) {
+          setError('Please check email and password');
+          return;
+        }
+
+        dispatch({ type: 'LOG_IN' });
+        navigate('/', { replace: true });
+      } catch (e) {
+        return e;
+      }
+    },
+    [dispatch, values, navigate]
+  );
+
+  const handleCloseError = useCallback(() => setError(undefined), []);
+
+  return (
+    <AppForm onSubmit={handleFormSubmit}>
+      <Stack>
+        <Typography variant="h5" sx={{ mb: 3 }}>
+          Sign In
+        </Typography>
+        <TextField
+          required
+          label="Username"
+          name="username"
+          inputProps={{ autoCapitalize: 'none' }}
+          value={values.username}
+          error={fieldHasError('username')}
+          helperText={fieldGetError('username') || ' '}
+          onChange={onFieldChange}
+          sx={{ mt: 0 }}
+          {...SHARED_CONTROL_PROPS}
+        />
+        <TextField
+          required
+          type={showPassword ? 'text' : 'password'}
+          label="Password"
+          name="password"
+          value={values.password}
+          error={fieldHasError('password')}
+          helperText={fieldGetError('password') || ' '}
+          onChange={onFieldChange}
+          sx={{ mt: 0 }}
+          {...SHARED_CONTROL_PROPS}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <AppIconButton
+                  aria-label="toggle password visibility"
+                  icon={showPassword ? 'visibilityon' : 'visibilityoff'}
+                  title={showPassword ? 'Hide Password' : 'Show Password'}
+                  onClick={handleShowPasswordClick}
+                  onMouseDown={eventPreventDefault}
+                />
+              </InputAdornment>
+            ),
+          }}
+        />
+        {error ? (
+          <AppAlert severity="error" onClose={handleCloseError}>
+            {error}
+          </AppAlert>
+        ) : null}
+        <AppButton type="submit" color="primary" disabled={!formState.isValid} sx={{ mx: 0, mt: 0 }}>
+          Sign In
+        </AppButton>
+        <Grid container justifyContent="end" alignItems="center">
+          <Button variant="text" color="secondary" component={AppLink} to="/recovery/password">
+            Forgot Password?
+          </Button>
+        </Grid>
+      </Stack>
+    </AppForm>
+  );
+};
+
+export default LoginView;
