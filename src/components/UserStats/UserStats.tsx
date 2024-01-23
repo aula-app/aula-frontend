aa3c519def1ed54fca0e2d433c1d88ce627d8128
Eduardo Liron <eduardoliron@gmail.com>
2024-01-23 16:46:01 -0300

    fix(UseStats): change import paths to absolute

--- src/components/UserStats/UserStats.tsx
+++ src/components/UserStats/UserStats.tsx
@@ -1,6 +1,6 @@
 import { Grid } from '@mui/material';
 import { PhaseButton } from '../PhaseButton';
-import phases from '../../utils/phases';
+import phases from '@/utils/phases';
 
 const displayPhases = Object.keys(Object.freeze(phases)) as Array<keyof typeof phases>;
 if(displayPhases.includes('result')) displayPhases.splice(displayPhases.indexOf('result'), 1);
