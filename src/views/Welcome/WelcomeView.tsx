b5407d125b85e2b08a57cd41f19e78952931f169
Eduardo Liron <eduardoliron@gmail.com>
2024-02-06 09:58:48 -0300

    fix(WelcomeView): improve dashboard access

--- src/views/Welcome/WelcomeView.tsx
+++ src/views/Welcome/WelcomeView.tsx
@@ -1,4 +1,4 @@
-import { Paper, Stack, Typography } from '@mui/material';
+import { Box, Button, Divider, Paper, Stack, Typography } from '@mui/material';
 import { RoomCards } from '@/components/RoomCards';
 import { NotificationsBar } from '@/components/NotificationsBar';
 import { UserStats } from '@/components/UserStats';
@@ -11,45 +11,69 @@ const WelcomeView = () => {
 
   const handleScroll = (event: React.UIEvent<HTMLElement>) => {
     let currentScroll = event.currentTarget.scrollTop;
-    if (currentScroll === 0 || scrollTop > currentScroll) {
+    if (currentScroll === 0) {
       setNotificationsBar(true);
-    } else {
+    } else if (currentScroll !== scrollTop) {
       setNotificationsBar(false);
     }
     setScrollTop(currentScroll);
   };
 
+  const toggleNotifications = () => {
+    const newValue = !showNotificationsBar;
+    setNotificationsBar(newValue);
+  };
+
   return (
-    <Stack height="100%" flexGrow={1}>
-      <Stack sx={{
-        alignItems: 'center',
-        bgcolor: blueGrey[50],
-        maxHeight: `${showNotificationsBar ? 50 : 0}px`,
-        overflow: 'clip',
-        transition: 'all 1s ease-in-out'
+    <Stack
+      height="100%"
+      flexGrow={1}
+      position="relative"
+      onScroll={handleScroll}
+      onMouseEnter={() => setNotificationsBar(true)}
+      sx={{
+        px: 2,
+        overflowY: 'auto',
+        scrollSnapType: 'y mandatory',
       }}
+    >
+      <Stack
+        position="fixed"
+        top={56}
+        left={0}
+        sx={{
+          alignItems: 'center',
+          bgcolor: blueGrey[50],
+          maxHeight: `${showNotificationsBar ? 250 : 25}px`,
+          overflow: 'clip',
+          transition: 'all .5s ease-in-out',
+          zIndex: 300,
+        }}
       >
         <NotificationsBar />
-      </Stack>
-      <Paper
-        onScroll={handleScroll}
-        elevation={8}
-        sx={{
-          px: 2,
-          overflowY: 'auto',
-          scrollSnapType: 'y mandatory'
-        }}>
-        <Stack sx={{scrollSnapAlign: 'start'}}>
-          <Typography variant="h4" pt={2} pb={1}>
+        <Stack p={2} pt={0}>
+          <Typography variant="h4" pt={2}>
             Your Activity
           </Typography>
           <UserStats />
         </Stack>
-        <Typography variant="h4" pt={2} sx={{scrollSnapAlign: 'start'}}>
-          Rooms
-        </Typography>
-        <RoomCards />
-      </Paper>
+        <Box position="absolute" bottom={0} width="100%" bgcolor={blueGrey[50]}>
+          <Button sx={{ width: '100%', py: 1 }} onClick={toggleNotifications}>
+            <Divider sx={{ width: '50%' }} variant="middle" />
+          </Button>
+        </Box>
+      </Stack>
+      <Typography
+        variant="h4"
+        pt={scrollTop === 0 && showNotificationsBar ? 30 : 4}
+        sx={{
+          scrollSnapAlign: 'start',
+          transition: 'all .5s ease-in-out'
+
+        }}>
+        Rooms
+      </Typography>
+      <RoomCards />
     </Stack>
   );
 };
