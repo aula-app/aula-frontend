c3999f413119b391e6817a268ca03dd71f342f83
Eduardo Liron <eduardoliron@gmail.com>
2024-02-08 12:21:04 -0300

    feat(WildIdea): Get correct User Name and limit name size with ellipsis

--- src/components/WildIdea/WildIdea.tsx
+++ src/components/WildIdea/WildIdea.tsx
@@ -3,22 +3,29 @@ import { Chip, Stack, Typography } from '@mui/material';
 import { IdeaBubble } from '../IdeaBubble';
 
 interface IdeaBubbleProps {
-  title?: string;
+  username: string;
   text: string;
 }
 
-export const WildIdea = ({ title, text }: IdeaBubbleProps) => {
+export const WildIdea = ({ username, text }: IdeaBubbleProps) => {
   return (
     <Stack mb={2} mx={1} sx={{scrollSnapAlign: 'center'}}>
-      <IdeaBubble title={title} text={text} />
+      <IdeaBubble text={text} />
       <Stack direction="row" alignItems="center">
         <AccountCircle sx={{ fontSize: '3em' }} />
-        <Stack ml={1}>
+        <Stack ml={1} maxWidth='50%' overflow='hidden'>
           <Typography variant="caption" lineHeight={1.5}>
             date
           </Typography>
-          <Typography variant="overline" fontWeight={700} lineHeight={1.5}>
-            username
+          <Typography
+            variant="overline"
+            overflow='hidden'
+            textOverflow="ellipsis"
+            fontWeight={700}
+            lineHeight={1.5}
+            maxWidth='100%'
+            >
+            {username}
           </Typography>
         </Stack>
         <Stack flexGrow={1} pr={1} direction="row" alignItems="center" justifyContent="flex-end">
