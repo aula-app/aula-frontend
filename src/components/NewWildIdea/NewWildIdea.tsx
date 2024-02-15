696d9649ebf5eea133512f74faf025fc94deca27
Eduardo Liron <eduardoliron@gmail.com>
2024-02-15 10:40:10 -0300

    feat(NewWildIdea): set new wild Idea apearence

--- src/components/NewWildIdea/NewWildIdea.tsx
+++ src/components/NewWildIdea/NewWildIdea.tsx
@@ -1,5 +1,5 @@
 import { AccountCircle } from '@mui/icons-material';
-import { Button, Stack, TextareaAutosize } from '@mui/material';
+import { Box, Button, Stack, TextField } from '@mui/material';
 
 export const NewWildIdea = () => {
   return (
@@ -8,7 +8,25 @@ export const NewWildIdea = () => {
         <AccountCircle sx={{ fontSize: '3em', mr: 'auto' }} />
         <Button variant="contained">Submit</Button>
       </Stack>
-      <TextareaAutosize minRows={6} placeholder="What is your idea?" />
+      <Stack position='relative'>
+        <TextField
+          variant='filled'
+          multiline
+          minRows={6}
+          placeholder="What is your idea?" />
+        <Box
+          sx={{
+            position: 'absolute',
+            top: 0,
+            left: '25px',
+            border: `8px solid #eee`,
+            borderTopColor: 'transparent',
+            borderRightColor: 'transparent',
+            transformOrigin: 'bottom left',
+            transform: 'translateY(-100%)'
+          }}
+          />
+      </Stack>
     </Stack>
   );
 };
