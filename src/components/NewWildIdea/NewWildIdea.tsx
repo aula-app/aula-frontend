b0949105513db5bfbbef5c1b1190a9f67439b630
Eduardo Liron <eduardoliron@gmail.com>
2024-02-16 10:27:29 -0300

    wip(NewWildIdea): implement idea adition dialog – waiting for backend

--- src/components/NewWildIdea/NewWildIdea.tsx
+++ src/components/NewWildIdea/NewWildIdea.tsx
@@ -1,12 +1,36 @@
 import { AccountCircle } from '@mui/icons-material';
+import { localStorageGet } from '@/utils/localStorage';
 import { Box, Button, Stack, TextField } from '@mui/material';
+import { useCallback } from 'react';
 
 export const NewWildIdea = () => {
+
+  const jwt_token = localStorageGet('token');
+
+  const submitNewIdea = async () => {
+    const data = await (
+        await fetch(
+          import.meta.env.VITE_APP_API_URL + '/api/controllers/add_idea.php',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + jwt_token
+            },
+            body: JSON.stringify(
+              {'content': 'test',
+              'room_id': 2,
+            })
+          })).json();
+
+        console.log(data)
+  }
+
   return (
     <Stack p={2}>
       <Stack pb={2} direction="row">
         <AccountCircle sx={{ fontSize: '3em', mr: 'auto' }} />
-        <Button variant="contained">Submit</Button>
+        <Button variant="contained" onClick={submitNewIdea}>Submit</Button>
       </Stack>
       <Stack position='relative'>
         <TextField
