bf22c633bcdf90b223efcf86f5c5fb850b5ea470
Eduardo Liron <eduardoliron@gmail.com>
2024-03-05 13:45:51 +0100

    feat(requests): add generic request method for login routine

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,33 +1,40 @@
 import { parseJwt } from './jwt';
 import { localStorageGet } from './localStorage';
 
+interface Header {
+  'Content-Type': 'application/json';
+  'Authorization'?: string;
+}
+
 const jwt_token = localStorageGet('token');
 const jwt_payload = jwt_token ? parseJwt(jwt_token) : null;
 
 export const databaseRequest = async (table: string, requestData: Object) => {
-  if (!jwt_payload) return false;
+  const header = new Headers();
+  header.set('Content-Type', 'application/json');
+  if(table !== 'login') header.set('Authorization', `Bearer ${jwt_token}`);
+  if(table !== 'login') requestData = {...requestData, user_id: jwt_payload.user_id};
+
+  if (!jwt_payload) return null;
   try {
     const response = await (
       await fetch(`${import.meta.env.VITE_APP_API_URL}/api/controllers/${table}.php`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + jwt_token,
-      },
-      body: JSON.stringify({
-        user_id: jwt_payload.user_id,
-        ...requestData,
-      }),
-    })).json();
+        method: 'POST',
+        headers: header,
+        body: JSON.stringify(
+          requestData
+        ),
+      })
+    ).json();
 
     if (response && response.success) {
-      return true;
+      return response;
     } else {
-      console.log('error');
-      return false;
+      console.log('failed');
+      return null;
     }
   } catch (e) {
-    console.log(e)
-    return false;
+    console.log(e);
+    return null;
   }
 };
