31665b974ba783fafa42ccca4db6adfcf78821d4
Eduardo Liron <eduardoliron@gmail.com>
2024-03-07 09:57:44 +0100

    fix(ErrorMessages): fix duplicated running of dispatch event on button click

--- src/components/ErrorMessages/ErrorMessages.tsx
+++ src/components/ErrorMessages/ErrorMessages.tsx
@@ -1,5 +1,5 @@
 import { useAppStore } from '@/store';
-import { Alert, Box, Snackbar } from '@mui/material';
+import { Alert, Box, Snackbar, Stack } from '@mui/material';
 import { useEffect, useState } from 'react';
 
 /**
@@ -13,36 +13,24 @@ const ErrorMessages = () => {
   const handleClose = () => {
     setOpen(false);
     const interval = setTimeout(() => {
-      dispatch({ type: 'REMOVE_ERROR', index: 0 });
+      dispatch({ type: 'REMOVE_ALL_ERRORS' });
     }, 300);
   };
 
   useEffect(() => {
-    if(state.errors.length > 0) setOpen(true);
-  }, [JSON.stringify(state.errors)])
+    if (state.errors.length > 0) setOpen(true);
+  }, [JSON.stringify(state.errors)]);
 
   return (
-    <>
-    <Box
-      top={0}
-      position="absolute"
-      zIndex={10000}>
-      {state.errors.map(error => <>{error}<br /></>)}
-    </Box>
-    <Snackbar
-      anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-      autoHideDuration={5000}
-      open={open}
-
-      >
-      <Alert
-         onClose={handleClose}
-         variant='filled'
-         severity='error'>
-        {state.errors[0]}
-      </Alert>
+    <Snackbar anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }} autoHideDuration={2000} open={open}>
+      <div>
+        {state.errors.map((message, i) => (
+          <Alert onClose={handleClose} variant="filled" severity="error" key={i} sx={{mt: 1}}>
+            <Box>{message}</Box>
+          </Alert>
+        ))}
+      </div>
     </Snackbar>
-    </>
   );
 };
 
