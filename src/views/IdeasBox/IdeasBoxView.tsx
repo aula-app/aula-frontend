6d43c76c595dafe8c9e6f148459b3978e9e2c6bf
Eduardo Liron <eduardoliron@gmail.com>
2024-03-07 11:32:09 +0100

    rebase

--- src/views/IdeasBox/IdeasBoxView.tsx
+++ src/views/IdeasBox/IdeasBoxView.tsx
@@ -1,34 +1,24 @@
-import { Box, Stack, Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import { IdeaBox } from '@/components/IdeaBox';
 import { IdeaCards } from '@/components/IdeaCards';
 
 /** * Renders "IdeasBox" view
- * url: /room/:roomId/ideas-box/:boxId
+ * url: /room/:rommId/ideas-box/:boxId
  */
 const IdeasBoxView = () => {
   return (
-    <Box
-      height="100%"
-      flexGrow={1}
-      position="relative"
-      px={1}
-      py={2}
-      sx={{
-        overflowY: 'auto',
-        scrollSnapType: 'y mandatory',
-      }}
-    >
-        <IdeaBox noCategories />
-        <Typography variant="h6" p={2}>
-          X ideas
-        </Typography>
+    <Stack px={1} py={2} sx={{scrollSnapAlign: 'center'}}>
+      <IdeaBox noCategories />
+      <Stack>
+        <Typography variant='h6' p={2}>X ideas</Typography>
         <IdeaCards />
         <IdeaCards variant="approved" />
         <IdeaCards variant="dismissed" />
         <IdeaCards variant="voting" />
         <IdeaCards variant="voted" />
         <IdeaCards variant="rejected" />
-    </Box>
+      </Stack>
+    </Stack>
   );
 };
 
