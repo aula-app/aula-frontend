bed891f5ce895a34004cd8ede55fb1ed2172034a
Eduardo Liron <eduardoliron@gmail.com>
2024-03-22 17:08:12 +0100

    fix(RoomSettings): add view for Room Settings

--- src/views/Rooms/RoomsView.tsx
+++ src/views/Rooms/RoomsView.tsx
@@ -6,18 +6,17 @@ import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-
-/** * Renders "Welcome" view
+/** * Renders "Rooms" view
  * url: /
  */
 const RoomsView = () => {
   const params = useParams();
   const [rooms, setRooms] = useState({} as RoomsResponseType);
-  const columns:GridColDef[] = [
-    { field: 'room_name', headerName: 'Name', width: 260},
-    { field: 'description_internal', headerName: 'Internal Description', width: 300},
-    { field: 'description_public', headerName: 'Public Description', width: 300},
-  ]
+  const columns: GridColDef[] = [
+    { field: 'room_name', headerName: 'Name', width: 260 },
+    { field: 'description_internal', headerName: 'Internal Description', width: 300 },
+    { field: 'description_public', headerName: 'Public Description', width: 300 },
+  ];
 
   const roomsFetch = async () =>
     await databaseRequest('model', {
@@ -25,28 +24,34 @@ const RoomsView = () => {
       method: 'getRoomBaseData',
       arguments: { room_id: Number(params['room_id']) },
       decrypt: ['content', 'displayname'],
-    })
-    .then((response: RoomsResponseType) => setRooms(response));
+    }).then((response: RoomsResponseType) => {
+      setRooms(response)
+      console.log(response)
+    });
 
-  useEffect(() => { roomsFetch() },[]);
+  useEffect(() => {
+    roomsFetch();
+  }, []);
 
   return (
     <Stack direction="column" spacing={2}>
       <Typography variant="h4">Rooms</Typography>
 
-      <div style={{ width: '100%' }}>
-        <DataGrid
-          rows={rooms.data}
-          columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: { page: 0, pageSize: 10 },
-            },
-          }}
-          pageSizeOptions={[10, 20]}
-          checkboxSelection
-        />
-      </div>
+      {rooms.data && (
+        <div style={{ width: '100%' }}>
+          <DataGrid
+            rows={rooms.data}
+            columns={columns}
+            initialState={{
+              pagination: {
+                paginationModel: { page: 0, pageSize: 10 },
+              },
+            }}
+            pageSizeOptions={[10, 20]}
+            checkboxSelection
+          />
+        </div>
+      )}
     </Stack>
   );
 };
