9812a3d470782f11cf77052693df866da80c5f0d
Eduardo Liron <eduardoliron@gmail.com>
2024-03-25 10:44:17 +0100

    fix(PrivateRoutes): set redirect from bare /room path to /room/ideas

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes, useParams } from 'react-router-dom';
 import { NotFoundView } from '@/views';
 import AboutView from '@/views/About';
 import { WelcomeView } from '@/views/Welcome';
@@ -16,6 +16,13 @@ import { TextsView } from '@/views/Texts';
  * List of routes available only for authenticated users
  * Also renders the "Private Layout" composition
  */
+
+function DefaultRoom() {
+  // Get the userId param from the URL.
+  let { room_id } = useParams();
+  return (<Navigate to={`/room/${room_id}/ideas`} replace />)
+}
+
 const PrivateRoutes = () => (
   <Routes>
     <Route path="/" element={<WelcomeView />} />
@@ -23,7 +30,7 @@ const PrivateRoutes = () => (
     <Route path="user" element={<UserView />} />
     <Route path="about" element={<AboutView />} />,
     <Route path="rooms" element={<RoomsSettingView />} />,
-    <Route path="room/:room_id" element={<RoomView />} />,
+    <Route path="room/:room_id" element={<DefaultRoom />} />,
     <Route path="room/:room_id/ideas" element={<RoomView />} />,
     <Route path="room/:room_id/boxes" element={<RoomView />} />,
     <Route path="room/:room_id/idea-box/:box_id" element={<IdeasBoxView />} />,
