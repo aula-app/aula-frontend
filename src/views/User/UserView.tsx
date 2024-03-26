a618c34a4b1018fa6ff0505e5300ad5f41175cce
Eduardo Liron <eduardoliron@gmail.com>
2024-03-26 12:28:29 +0100

    feat(UserView): add getUserBaseData request

--- src/views/User/UserView.tsx
+++ src/views/User/UserView.tsx
@@ -2,43 +2,41 @@ import { Typography } from '@mui/material';
 import { Stack } from '@mui/system';
 import { localStorageGet } from '@/utils/localStorage';
 import { useEffect, useState } from 'react';
-
+import { databaseRequest } from '@/utils/requests';
+import { parseJwt } from '@/utils/jwt';
+import { UserResponseType, UserType } from '@/types/UserTypes';
 
 /** * Renders "User" view
  * url: /user
  */
 const UserView = () => {
-  const [, setData] = useState([] as any[]);
+  const [user, setUser] = useState<UserType | null>(null);
   const jwt_token = localStorageGet('token');
+  const jwt_payload = parseJwt(jwt_token);
+
+  const getUserInfo = async () =>
+    databaseRequest('model', {
+      model: 'User',
+      method: 'getUserBaseData',
+      arguments: { user_id: jwt_payload.user_id },
+      decrypt: ['displayname', 'username', 'email', 'about_me'],
+    }).then((response: UserResponseType) => setUser(response.data));
 
   useEffect(() => {
-    // fetch data
-    const dataFetch = async () => {
-      const data = await (
-        await fetch(
-          "/api/controllers/user.php",
-          {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': 'Bearer ' + jwt_token
-            },
-         }
-        )
-      ).json();
-
-      setData(data.data)
-    };
-
-    dataFetch();
-    },[jwt_token]);
+    getUserInfo();
+  }, []);
 
   return (
-    <Stack direction="column" spacing={2}>
+    <Stack width="100%" height="100%" overflow="hidden">
       <Typography variant="h4">User</Typography>
-
-      <div style={{ width: '100%' }}>
-      </div>
+      {user && (
+        <Stack p={2}>
+          <p>{user.username}</p>
+          <p>{user.displayname}</p>
+          <p>{user.email}</p>
+          <p>{user.about_me}</p>
+        </Stack>
+      )}
     </Stack>
   );
 };
