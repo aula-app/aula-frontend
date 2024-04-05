ff30be781329b78dea6ebc1cce74cb6fb510044c
Eduardo Liron <eduardoliron@gmail.com>
2024-04-05 16:41:58 +0200

    feat(ItemsTable): set navigation

--- src/views/Users/UsersView.tsx
+++ src/views/Users/UsersView.tsx
@@ -1,55 +1,42 @@
+import ItemsTable from '@/components/ItemsTable';
 import { UsersResponseType } from '@/types/UserTypes';
 import { databaseRequest } from '@/utils/requests';
 import { Typography } from '@mui/material';
 import { Stack } from '@mui/system';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
 
 /** * Renders "Rooms" view
  * url: /
  */
 const RoomsView = () => {
-  const params = useParams();
   const [users, setUsers] = useState({} as UsersResponseType);
-  const columns: GridColDef[] = [
-    { field: 'room_name', headerName: 'Name', width: 260 },
-    { field: 'description_internal', headerName: 'Internal Description', width: 300 },
-    { field: 'description_public', headerName: 'Public Description', width: 300 },
-  ];
 
-  const usersFetch = async () =>
+  const usersFetch = async (page: number, limit: number) =>
     await databaseRequest('model', {
       model: 'User',
       method: 'getUsers',
-      arguments: { limit: 20, offset: 20 },
+      arguments: { limit: limit, offset: page * limit },
       decrypt: ['realname', 'username', 'displayname', 'email'],
     }).then((response: UsersResponseType) => {
       setUsers(response);
     });
 
   useEffect(() => {
-    usersFetch();
+    usersFetch(0, 20);
   }, []);
 
   return (
-    <Stack direction="column" spacing={2}>
-      <Typography variant="h4">Rooms</Typography>
+    <Stack direction="column" height="100%">
+      <Typography variant="h4" sx={{ p: 2, pb: 0 }}>
+        Users
+      </Typography>
 
       {users.data && (
-        <div style={{ width: '100%' }}>
-          <DataGrid
-            rows={users.data}
-            columns={columns}
-            initialState={{
-              pagination: {
-                paginationModel: { page: 0, pageSize: 10 },
-              },
-            }}
-            pageSizeOptions={[10, 20]}
-            checkboxSelection
+        <ItemsTable
+          items={users}
+          displayRows={['displayname', 'email']}
+          reloadMethod={usersFetch}
           />
-        </div>
       )}
     </Stack>
   );
