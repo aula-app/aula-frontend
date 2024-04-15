31299714c7210f172ea30b938303a3e64c64b29b
Eduardo Liron <eduardoliron@gmail.com>
2024-04-15 17:56:00 -0300

    add missing files

---
+++ src/types/TableTypes.ts
@@ -0,0 +1,25 @@
+import { ObjectPropByName } from './Generics';
+import { UserTypeKeys } from './UserTypes';
+
+export interface TableRow {
+  id: number;
+  name: UserTypeKeys;
+  displayName: string;
+  encryption: boolean;
+}
+
+export interface TableOptions {
+  model: 'User' | 'Groups';
+  method: 'getUsers' | 'getGroups';
+  page: number;
+  limit: number;
+  orderBy: UserTypeKeys;
+  orderAsc: boolean;
+  rows: TableRow[];
+}
+
+export interface TableResponseType {
+  success: Boolean;
+  count: Number;
+  data: ObjectPropByName[];
+}
