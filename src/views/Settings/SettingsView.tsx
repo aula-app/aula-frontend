618647c1b5f4b84ee31db3b437d7e7e10ef004ec
Eduardo Liron <eduardoliron@gmail.com>
2024-04-15 15:33:56 -0300

    feat(SettingsView): put itemsTable directly into SettingsView

--- src/views/Settings/SettingsView.tsx
+++ src/views/Settings/SettingsView.tsx
@@ -1,14 +1,31 @@
-import ItemsTable from '@/components/ItemsTable';
-import { Typography } from '@mui/material';
-import { Stack } from '@mui/system';
-import UsersView from './Users';
 import { useParams } from 'react-router-dom';
+import {
+  Fab,
+  InputAdornment,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableSortLabel,
+  TextField,
+  Typography,
+} from '@mui/material';
+import UsersView from './Users';
 import GroupsView from './Groups';
 import IdeasView from './Ideas';
 import RoomsView from './Rooms';
-import { SettingsType } from '@/types/SettingsTypes';
-import { NotFoundView } from '..';
 import TextsView from './Texts';
+import { NotFoundView } from '..';
+import Tables from '@/utils/tables.json';
+import { SettingsType } from '@/types/SettingsTypes';
+import { Add, Search } from '@mui/icons-material';
+import { databaseRequest } from '@/utils/requests';
+import { TableOptions, TableResponseType } from '@/types/TableTypes';
+import { useEffect, useState } from 'react';
+
+const DEFAULT_LIMIT = Math.floor((window.screen.height - 220) / 34) || 10;
 
 /** * Renders default "Settings" view
  * urls: /settings/groups, /settings/ideas, /settings/rooms, /settings/texts, /settings/users
@@ -22,15 +39,95 @@ const SettingsView = () => {
     users: <UsersView />,
   };
 
-  const { setting_name } = useParams();
-  const isSettings = (param: any): param is SettingsType => Object.keys(pages).includes(param);
+  const { setting_name } = useParams() as { setting_name: SettingsType };
+  const isSettings = (param: SettingsType) => Object.keys(pages).includes(param);
+  const options = Tables[setting_name] as TableOptions;
+  const columns = options.rows.map((value) => value.name);
+  const decrypt = columns.filter((column, key) => options.rows[key].encryption);
+
+  const [items, setItems] = useState({} as TableResponseType);
+  const [page, setPage] = useState(0);
+  const [limit, setLimit] = useState(DEFAULT_LIMIT);
+  const [orderBy, setOrder] = useState(options.rows[0]['id']);
+  const [orderAsc, setOrderAsc] = useState(true);
+
+  const dataFetch = async () =>
+    await databaseRequest('model', {
+      model: options.model,
+      method: options.method,
+      arguments: {
+        limit: limit,
+        offset: page * limit,
+        orderby: orderBy,
+        asc: Number(orderAsc),
+      },
+      decrypt: decrypt,
+    }).then((response: TableResponseType) => {
+      setItems(response);
+    });
+
+  useEffect(() => {
+    dataFetch();
+  }, [page, limit, orderBy, orderAsc]);
+
+  const handleOrder = (col: number) => {
+    if (orderBy === col) setOrderAsc(!orderAsc);
+    setOrder(col);
+  };
 
   return isSettings(setting_name) ? (
     <Stack direction="column" height="100%">
+      <Fab aria-label="add" color="primary" sx={{ position: 'absolute', bottom: 60, right: 20 }}>
+        <Add />
+      </Fab>
       <Typography variant="h4" sx={{ p: 2, pb: 0, textTransform: 'capitalize' }}>
         {setting_name}
       </Typography>
-      <ItemsTable table={setting_name} />
+      <TextField
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Search />
+            </InputAdornment>
+          ),
+        }}
+        variant="standard"
+        sx={{ px: 2 }}
+      />
+      {/* <ItemsTable items={items.data} /> */}
+      <Table stickyHeader size="small">
+        <TableHead>
+          <TableRow>
+            {columns.map((column, key) => (
+              <TableCell key={column} sx={{ whiteSpace: 'nowrap' }}>
+                <TableSortLabel
+                  active={orderBy === options.rows[key].id}
+                  direction={orderAsc ? 'asc' : 'desc'}
+                  onClick={() => handleOrder(options.rows[key].id)}
+                >
+                  {options.rows[key].displayName}
+                </TableSortLabel>
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        {items.data && (
+            <TableBody>
+              {items.data.map((row) => (
+                <TableRow key={row.id}>
+                  {columns.map((column) => (
+                    <TableCell
+                      key={`${column}-${row.id}`}
+                      sx={{ overflow: 'clip', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}
+                    >
+                      {row[column]}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          )}
+      </Table>
     </Stack>
   ) : (
     <NotFoundView />
