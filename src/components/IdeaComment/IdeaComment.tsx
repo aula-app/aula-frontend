2eeda57b737ee662201c07f7a7881814f25b8f34
Eduardo Liron <eduardoliron@gmail.com>
2024-04-29 14:27:22 -0300

    add more space between idea comments

--- src/components/IdeaComment/IdeaComment.tsx
+++ src/components/IdeaComment/IdeaComment.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 export const Idea = ({ comment, onReload, disabled = false }: Props) => {
   return (
-    <Stack mb={1}>
+    <Stack mb={2}>
       <IdeaBubble bubbleInfo={comment} id={comment.id} onReload={onReload} disabled={disabled} />
       <Stack direction="row" alignItems="center" mt="-20px">
         <AccountCircle sx={{ fontSize: '2em' }} />
