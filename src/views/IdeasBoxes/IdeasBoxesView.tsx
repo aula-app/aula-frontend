c9cb340857fe3a4872f64ebcfb99f8a902414dc3
Eduardo Liron <eduardoliron@gmail.com>
2024-05-20 22:03:48 -0300

    feat(Room): add grid to Room cards and Room view

--- src/views/IdeasBoxes/IdeasBoxesView.tsx
+++ src/views/IdeasBoxes/IdeasBoxesView.tsx
@@ -1,4 +1,4 @@
-import { Stack } from '@mui/material';
+import { Grid, Stack } from '@mui/material';
 import { IdeaBox } from '@/components/IdeaBox';
 import { BoxType } from '@/types/BoxTypes';
 import { AppLink } from '@/components';
@@ -14,13 +14,15 @@ interface BoxesProps {
 const IdeasBoxView = ({ boxes }: BoxesProps) => {
   const params = useParams();
   return (
-    <Stack px={1} py={2} sx={{ scrollSnapAlign: 'center' }}>
+    <Grid container spacing={2}>
       {boxes.map((box) => (
-        <AppLink to={`/room/${params.room_id}/idea-box/${box.id}`} mb={2} key={box.id}>
-          <IdeaBox box={box} />
-        </AppLink>
+        <Grid key={box.id} item xs={12} sm={6} md={4} lg={3} xl={2} sx={{ scrollSnapAlign: 'center' }}>
+          <AppLink to={`/room/${params.room_id}/idea-box/${box.id}`} mb={2} key={box.id}>
+            <IdeaBox box={box} />
+          </AppLink>
+        </Grid>
       ))}
-    </Stack>
+    </Grid>
   );
 };
 
