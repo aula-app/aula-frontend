cdbb2b72096fd5322e3a4e969d0d63b72d738d4a
Eduardo Liron <eduardoliron@gmail.com>
2024-06-03 17:42:00 -0300

    wip(MessagesView): fetch request to getMessagesByUser returned in error

--- src/views/Messages/MessagesView.tsx
+++ src/views/Messages/MessagesView.tsx
@@ -1,6 +1,10 @@
 import { AppIcon } from '@/components';
 import MessageCard from '@/components/MessageCard';
+import { localStorageGet } from '@/utils';
+import { parseJwt } from '@/utils/jwt';
+import { databaseRequest } from '@/utils/requests';
 import { IconButton, Stack, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 /**
  * Renders "Messages" view
@@ -8,6 +12,24 @@ import { IconButton, Stack, Typography } from '@mui/material';
  */
 
 const MessagesView = () => {
+  const jwt_token = localStorageGet('token');
+  const jwt_payload = parseJwt(jwt_token);
+  const [messages, setMessages] = useState([])
+
+  const messageFetch = async () =>
+    await databaseRequest('model', {
+      model: 'Message',
+      method: 'getMessagesByUser',
+      arguments: { user_id: jwt_payload.user_id },
+      decrypt: ['content', 'displayname'],
+    }).then((response) => {
+      console.log(response);
+    });
+
+    useEffect(() => {
+      messageFetch();
+    }, []);
+    
   return (
     <Stack p={2} sx={{ overflowY: 'auto' }}>
       <Stack direction="row" alignItems="center" justifyContent="space-between">
