aa06fe97e0b7938f3ce21d31a11aa790f8e56dd7
Eduardo Liron <eduardoliron@gmail.com>
2024-06-05 07:53:57 -0300

    wip(style): change black to dark grey

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,34 +2,27 @@ import { PaletteOptions, SimplePaletteColorOptions } from '@mui/material';
 
 const COLOR_PRIMARY: SimplePaletteColorOptions = {
   main: '#7DE293',
-  contrastText: '#1C1D1F',
 };
 
 const COLOR_SECONDARY: SimplePaletteColorOptions = {
   main: '#a6a6a6',
-  contrastText: '#1C1D1F',
 };
 
 const COLOR_ERROR: SimplePaletteColorOptions = {
   main: '#EF9A9A',
-  contrastText: '#1C1D1F'
-}
+};
 
 const COLOR_WARNING: SimplePaletteColorOptions = {
   main: '#ffdc82',
-  contrastText: '#1C1D1F'
-}
+};
 
 const COLOR_INFO: SimplePaletteColorOptions = {
   main: '#00c8ff',
-  contrastText: '#1C1D1F'
-}
+};
 
 const COLOR_SUCCESS: SimplePaletteColorOptions = {
   main: '#93cf4b',
-  contrastText: '#1C1D1F'
-}
-
+};
 
 /**
  * MUI colors set to use in theme.palette
