c77e508db7bab7a48f7d91ed8ea3dab418a80891
Eduardo Liron <eduardoliron@gmail.com>
2024-06-24 15:35:00 -0300

    feat(IdeaBox): new idea box layout

--- src/components/IdeaBox/IdeaBox.tsx
+++ src/components/IdeaBox/IdeaBox.tsx
@@ -1,60 +1,73 @@
-import { CardMedia, Stack, Typography } from '@mui/material';
+import { Box, CardMedia, Stack, Typography } from '@mui/material';
 import { Card, CardContent } from '@mui/material';
 import { phases } from '@/utils';
 import { BoxType } from '@/types/BoxTypes';
+import { RoomPhases } from '@/types/RoomTypes';
+import AppIcon from '../AppIcon';
 
 interface IdeaBoxProps {
   box: BoxType;
 }
 
-const displayPhases = Object.keys(Object.freeze(phases)) as Array<keyof typeof phases>;
-
 const IdeaBox = ({ box }: IdeaBoxProps) => {
+  const phaseNames = Object.keys(phases) as RoomPhases[];
+  const currentPhase = phaseNames.filter((key: RoomPhases) => phases[key].phase_id === box.phase_id)[0] as RoomPhases;
   return (
     <Card sx={{ borderRadius: '25px', scrollSnapAlign: 'center' }} variant="outlined">
-      <CardContent>
-        <Stack direction="row" mb={2} alignItems="center" spacing={2}>
-          {displayPhases.map((phase) => {
-            const isCurrentPhase = phases[phase].phase_id === box.phase_id;
-            return phase !== 'wild' && (
-              <Stack
-                className={isCurrentPhase ? "noVolume noSpace" : "noPrint"}
-                width={isCurrentPhase ? 'auto' : '1.5em'}
-                flexGrow={isCurrentPhase ? 1 : 0}
-                height="1.5rem"
-                borderRadius={999}
-                bgcolor={phases[phase].color}
-                alignItems="center"
-                justifyContent="center"
-                key={phase}
-              >
-                <Typography variant="caption">{isCurrentPhase ? phases[phase].name : ''}</Typography>
-              </Stack>
-            );
-          })}
+      <Stack
+        width="100%"
+        height="3rem"
+        alignItems="center"
+        direction="row"
+        bgcolor={phases[currentPhase].color}
+        p={1}
+        pr={2}
+      >
+        <Stack
+          bgcolor="#fff"
+          borderRadius={999}
+          height="100%"
+          sx={{ aspectRatio: 1 }}
+          justifyContent="center"
+          alignItems="center"
+          mr="auto"
+        >
+          <AppIcon icon="box" />
         </Stack>
-        <CardMedia
-          component="img"
-          height="194"
-          image="/img/aula-room.png"
-          alt="bg image"
-          sx={{ borderRadius: '10px' }}
-        />
-        {/* { !noCategories &&
-            <Stack direction="row" mt={3} alignItems="center">
-              <CurrentIcon />
-              <Typography>3</Typography>
-              <Stack flexGrow={1} pr={1} direction="row" alignItems="center" justifyContent="flex-end">
-                <Chip label="category" color="warning" />
-              </Stack>
-            </Stack>
-          } */}
-        <Typography variant="h6" sx={{ mt: 1.5 }} noWrap>
+        <AppIcon icon={phases[currentPhase].icon} size="small" sx={{ mx: 1 }} />
+        <Typography variant="caption">{phases[currentPhase].name} phase</Typography>
+      </Stack>
+      <CardContent>
+        <Typography variant="h6" noWrap>
           {box.name}
         </Typography>
-        <Typography sx={{ mt: 0.5, mb: -1 }} variant="body2" noWrap>
+        <Typography variant="body2" noWrap>
           {box.description_public}
         </Typography>
+        <Box
+          mt={2}
+          position="relative"
+          borderRadius={999}
+          width="100%"
+          height="1.5rem"
+          bgcolor={phases[currentPhase].color}
+          overflow="clip"
+        >
+          <Box bgcolor={phases[currentPhase].baseColor[300]} position="absolute" left={0} height="100%" width="50%" />
+          <Stack
+            direction="row"
+            position="absolute"
+            left={0}
+            height="100%"
+            width="100%"
+            alignItems="center"
+            justifyContent="end"
+            px={2}
+          >
+            <AppIcon icon="clock" size="small" sx={{ mx: .5 }} />
+            <Typography variant="caption">Phase ends in 3 days</Typography>
+          </Stack>
+        </Box>
       </CardContent>
     </Card>
   );
--- src/utils/phases.ts
+++ src/utils/phases.ts
@@ -1,11 +1,22 @@
+import { ObjectPropByName } from '@/types/Generics';
 import { RoomPhases } from '@/types/RoomTypes';
 import { green, amber, deepPurple, deepOrange, blue, grey, red } from '@mui/material/colors';
+
+interface phaseType {
+  name: string,
+  description: string,
+  color: string,
+  baseColor: ObjectPropByName,
+  icon: string,
+  phase_id: number,
+}
 
 export const allPhases = {
   wild: {
     name: 'Wild Ideas',
     description: 'Wild Ideas',
     color: blue[100],
+    baseColor: blue,
     icon: 'idea',
     phase_id: 0,
   },
@@ -13,6 +24,7 @@ export const allPhases = {
     name: 'Discussion',
     description: 'Ideas in Discussion',
     color: deepPurple[100],
+    baseColor: deepPurple,
     icon: 'discussion',
     phase_id: 10,
   },
@@ -20,6 +32,7 @@ export const allPhases = {
     name: 'Approval',
     description: 'Ideas Under Approval',
     color: deepOrange[100],
+    baseColor: deepOrange,
     icon: 'approval',
     phase_id: 20,
   },
@@ -27,6 +40,7 @@ export const allPhases = {
     name: 'Voting',
     description: 'Ideas on Voting',
     color: amber[100],
+    baseColor: amber,
     icon: 'vote',
     phase_id: 30,
   },
@@ -34,6 +48,7 @@ export const allPhases = {
     name: 'Results',
     description: 'Idea Results',
     color: green[100],
+    baseColor: green,
     icon: 'chart',
     phase_id: 40,
   },
@@ -41,6 +56,7 @@ export const allPhases = {
     name: 'Successful',
     description: 'Successful Ideas',
     color: green[100],
+    baseColor: green,
     icon: 'check',
     phase_id: 40,
   },
@@ -48,6 +64,7 @@ export const allPhases = {
     name: 'Rejected',
     description: 'Rejected Ideas',
     color: red[100],
+    baseColor: red,
     icon: 'cancel',
     phase_id: 40,
   },
@@ -55,10 +72,11 @@ export const allPhases = {
     name: 'Dismissed',
     description: 'Ideas not feasible',
     color: grey[100],
+    baseColor: grey,
     icon: 'forbid',
     phase_id: 40,
   },
 };
 
-export const phases = (({ dismissal, success, reject, ...o }) => o)(allPhases) as Record<RoomPhases, any>;
+export const phases = (({ dismissal, success, reject, ...o }) => o)(allPhases) as Record<RoomPhases, phaseType>;
 export const dashboardPhases = (({ result, dismissal, ...o }) => o)(allPhases);
