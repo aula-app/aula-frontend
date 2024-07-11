201bdeca822922ebd87d5dee3c00570f5d480bcc
Eduardo Liron <eduardoliron@gmail.com>
2024-07-11 10:52:37 -0300

    feat(RoomCard): put phaseBar at bottom

--- src/components/RoomCard/RoomCard.tsx
+++ src/components/RoomCard/RoomCard.tsx
@@ -16,31 +16,30 @@ const displayPhases = Object.keys(phases) as Array<keyof typeof phases>;
  */
 const RoomCard = ({ room }: RoomCardProps) => {
   return (
-    <Card sx={{ borderRadius: '25px', aspectRatio: 1, overflow: 'clip' }} variant="outlined">
-      <Stack>
-      <CardContent sx={{flex: 1}}>
-        <AppLink
-          sx={{
-            textDecoration: 'none',
-            color: 'inherit',
-          }}
-          to={`/room/${room.id}/phase/0`}
-        >
-          <Stack>
-          <Typography variant="h6" noWrap>
-            {capitalize(room.room_name)}
-          </Typography>
-          <CardMedia
-            component="img"
-            image={room.id % 2 === 0 ? '/img/aula-room1.jpg' : '/img/aula-room.jpg'}
-            alt="bg image"
-            sx={{ borderRadius: '10px', mt: 1, mb: 2, objectFit: 'contain', flex: 1 }}
-          />
-          </Stack>
-        </AppLink>
-      </CardContent>
-      <PhaseBar room={room.id} />
-      </Stack>
+    <Card sx={{ borderRadius: '25px', aspectRatio: 1 }} variant="outlined">
+        <Stack height="100%">
+          <AppLink
+            component={Stack}
+            sx={{
+              textDecoration: 'none',
+              color: 'inherit',
+              flex: 1,
+              p: 2
+            }}
+            to={`/room/${room.id}/phase/0`}
+          >
+              <Typography variant="h6" noWrap>
+                {capitalize(room.room_name)}
+              </Typography>
+              <CardMedia
+                component="img"
+                image={room.id % 2 === 0 ? '/img/aula-room1.jpg' : '/img/aula-room.jpg'}
+                alt="bg image"
+                sx={{ borderRadius: '10px', objectFit: 'contain', flex: 1 }}
+              />
+          </AppLink>
+          <PhaseBar room={room.id} />
+        </Stack>
     </Card>
   );
 };
