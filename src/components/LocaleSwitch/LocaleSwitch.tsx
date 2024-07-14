41ceeac00abaaf6a4ced6483e6bfad93dff15aec
Eduardo Liron <eduardoliron@gmail.com>
2024-07-14 09:49:46 -0300

    create localeswitch component

---
+++ src/components/LocaleSwitch/LocaleSwitch.tsx
@@ -0,0 +1,29 @@
+import { FormControl, MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+
+/**
+ * Restyled Link for navigation in the App, support internal links by "to" prop and external links by "href" prop
+ * @component LocaleSwitch
+ */
+const LocaleSwitch = () => {
+  const { t, i18n } = useTranslation();
+
+  const handleChange = (event: SelectChangeEvent) => {
+    i18n.changeLanguage(event.target.value);
+  };
+
+  return (
+    <FormControl>
+      <Select variant="standard" value={i18n.language} onChange={handleChange}>
+        {i18n &&
+          Object.keys(i18n.services.resourceStore.data).map((lng) => (
+            <MenuItem value={lng} key={lng}>
+              {lng}
+            </MenuItem>
+          ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default LocaleSwitch;
--- src/layout/PublicLayout.tsx
+++ src/layout/PublicLayout.tsx
@@ -3,34 +3,22 @@ import { Box, Button, Divider, FormControl, Grid, MenuItem, Select, SelectChange
 import { AppLink, ErrorBoundary } from '@/components';
 import { useLocation } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import LocaleSwitch from '@/components/LocaleSwitch';
 
 const TITLE_PUBLIC = 'aula';
 
 const PublicLayout: FunctionComponent<PropsWithChildren> = ({ children }) => {
   document.title = TITLE_PUBLIC; // Also Update Tab Title
 
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
   const location = useLocation();
-
-  const handleChange = (event: SelectChangeEvent) => {
-    i18n.changeLanguage(event.target.value);
-  };
 
   return (
     <Stack bgcolor="#fff" mx="auto" width="100%" p={2} maxWidth="20rem" minHeight="100vh">
       <Stack>
         <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ pb: 2 }}>
           {toggleBackToSignIn()}
-          <FormControl sx={{ ml: 'auto' }}>
-            <Select variant="standard" value={i18n.language} onChange={handleChange}>
-              {i18n &&
-                Object.keys(i18n.services.resourceStore.data).map((lng) => (
-                  <MenuItem value={lng} key={lng}>
-                    {lng}
-                  </MenuItem>
-                ))}
-            </Select>
-          </FormControl>
+          <LocaleSwitch />
         </Stack>
         <Box sx={{ width: '100%', mb: 2 }}>
           <img src="/logo-text.svg" alt="aula" />
@@ -72,6 +60,7 @@ const PublicLayout: FunctionComponent<PropsWithChildren> = ({ children }) => {
         </AppLink>
       );
     }
+    return <Box></Box>;
   }
 
   function toggleSignUp() {
