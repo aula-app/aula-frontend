fc1c1fd57ce00d58ea47276b52f616186df68560
Eduardo Liron <eduardoliron@gmail.com>
2024-07-25 09:02:04 -0300

    adjust welcome view room sizing

--- src/views/cat.tsx
+++ src/views/cat.tsx
@@ -1,16 +1,20 @@
-import CategoryIcon, { CAT_ICONS, CategoryIconType } from '@/components/CategoryIcon/CategoryIcon';
-import { Grid } from '@mui/material';
+import { CAT_ICONS, CategoryIconType } from '@/components/CategoryIcon/CategoryIcon';
+import DefaultImage from '@/components/DefaultImages';
+import { Stack } from '@mui/material';
 
 const CatView = () => {
   const icons = Object.keys(CAT_ICONS) as CategoryIconType[];
   return (
-    <Grid container spacing={2}>
-      {icons.map((icon) => (
-        <Grid item xs={1} key={icon}>
-          <CategoryIcon icon={icon} size="xl" />
-        </Grid>
-      ))}
-    </Grid>
+    <Stack>
+      {/* <Grid container spacing={2} p={2}>
+        {icons.map((icon) => (
+          <Grid item xs={1} key={icon}>
+            <CategoryIcon icon={icon} size="xl" />
+          </Grid>
+        ))}
+      </Grid> */}
+      <DefaultImage />
+    </Stack>
   );
 };
 
