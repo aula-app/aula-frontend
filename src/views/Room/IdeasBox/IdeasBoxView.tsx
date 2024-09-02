15960f616a9f4629e795a32b67eaa598978e59e3
Eduardo Liron <eduardoliron@gmail.com>
2024-09-02 08:54:01 -0300

    fix(IdeasBoxView): fix idea selection component

--- src/views/Room/IdeasBox/IdeasBoxView.tsx
+++ src/views/Room/IdeasBox/IdeasBoxView.tsx
@@ -92,7 +92,7 @@ const IdeasBoxView = () => {
         </Stack>
         {boxIdeas && (
           <>
-            {checkPermissions(30) && <MoveData id={Number(params['box_id'])} scope="ideas" onClose={boxIdeasFetch} />}
+            {checkPermissions(30) && <MoveData id={Number(params['box_id'])} scope="boxes" onClose={boxIdeasFetch} />}
             <Grid container spacing={1} pt={1}>
               {boxIdeas.data &&
                 boxIdeas.data.map((idea, key) => (
