f8a8a1e16120715aa412a2f0415d4c1209961a18
Eduardo Liron <eduardoliron@gmail.com>
2024-09-03 16:48:09 -0300

    feat(COnfigView): organize configurations

--- src/views/Settings/Config/ConfigView.tsx
+++ src/views/Settings/Config/ConfigView.tsx
@@ -1,85 +1,54 @@
-import { Checkbox, FormControlLabel, FormGroup, Grid2 as Grid, Switch, TextField, Typography } from '@mui/material';
+import { AppIcon } from '@/components';
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
 import { Stack } from '@mui/system';
-import { LocalizationProvider, TimePicker } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import dayjs from 'dayjs';
-import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Categories from './Categories';
+import SchoolInfo from './SchoolInfo';
+import SystemSettings from './SystemSettings';
 
 /** * Renders "Config" view
  * url: /settings/config
  */
 const ConfigView = () => {
   const { t } = useTranslation();
-  const [weekDays, setWeekDays] = useState<number[]>([]);
-
-  var week = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
-
-  const toggleDay = (day: number) => {
-    weekDays.includes(day) ? setWeekDays([...weekDays.filter((i) => i !== day)]) : setWeekDays([...weekDays, day]);
-  };
 
   return (
     <Stack width="100%" height="100%" sx={{ overflowY: 'auto' }} gap={2} p={2}>
       <Typography variant="h4" pb={2}>
         {t('views.configuration')}
       </Typography>
-      <Stack gap={1}>
-        <Typography variant="h5" py={1}>
-          {t(`settings.school`)}
-        </Typography>
-        <TextField label={t(`settings.name`)} fullWidth />
-        <TextField label={t(`settings.description`)} minRows={4} multiline type="text" fullWidth />
-      </Stack>
-      <Stack>
-        <Typography variant="h5" py={1}>
-          {t(`settings.settings`)}
-        </Typography>
-        <Typography variant="h6">{t('settings.categories')}</Typography>
-        <Categories />
-      </Stack>
-      <Stack>
-        <Typography variant="h5" py={1}>
-          {t(`settings.time`)}
-        </Typography>
-        <Typography variant="h6">{t(`settings.workDays`)}</Typography>
-        <Grid component={FormGroup} container>
-          {week.map((day, i) => (
-            <Grid size="auto" key={i}>
-              <FormControlLabel
-                control={<Checkbox checked={weekDays.includes(i)} onChange={() => toggleDay(i)} />}
-                label={t(`week.${day}`)}
-              />
-            </Grid>
-          ))}
-        </Grid>
-        <Typography variant="h6">{t(`settings.workHours`)}</Typography>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <Grid container spacing={1}>
-            <Grid size="auto" pt={1}>
-              <TimePicker label={t(`settings.timeStart`)} defaultValue={dayjs('2022-04-17T08:00')} />
-            </Grid>
-            <Grid size="auto" pt={1}>
-              <TimePicker label={t(`settings.timeEnd`)} defaultValue={dayjs('2022-04-17T118:00')} />
-            </Grid>
-          </Grid>
-        </LocalizationProvider>
-      </Stack>
-      <Typography variant="h5" pt={1}>
-        {t(`settings.login`)}
-      </Typography>
-      <Grid container spacing={1}>
-        <Grid size="auto">
-          <FormControlLabel control={<Switch />} label={t(`settings.vacation`)} />
-        </Grid>
-        <Grid size="auto">
-          <FormControlLabel control={<Switch />} label={t(`settings.oAuth`)} />
-        </Grid>
-        <Grid size="auto">
-          <FormControlLabel control={<Switch />} label={t(`settings.registration`)} />
-        </Grid>
-      </Grid>
+      <SchoolInfo />
+      <Accordion>
+        <AccordionSummary expandIcon={<AppIcon icon="arrowdown" />} aria-controls="panel2-content" id="panel2-header">
+          <Typography variant="h5" py={1}>
+            {t(`settings.settings`)}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography variant="h6">{t('settings.categories')}</Typography>
+          <Categories />
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<AppIcon icon="arrowdown" />} aria-controls="panel2-content" id="panel2-header">
+          <Typography variant="h5" py={1}>
+            {t(`settings.system`)}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <SystemSettings />
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<AppIcon icon="arrowdown" />} aria-controls="panel2-content" id="panel2-header">
+          <Typography variant="h5" py={1}>
+            {t(`settings.advanced`)}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography variant="h6">Deactivate</Typography>
+        </AccordionDetails>
+      </Accordion>
     </Stack>
   );
 };
---
+++ src/views/Settings/Config/SystemSettings/SystemSettings.tsx
@@ -0,0 +1,70 @@
+import { Button, Checkbox, FormControlLabel, FormGroup, Grid2 as Grid, Stack, Switch, Typography } from '@mui/material';
+import { LocalizationProvider, TimePicker } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+const SysytemSettings = () => {
+  const { t } = useTranslation();
+
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  var week = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+
+  const toggleDay = (day: number) => {
+    weekDays.includes(day) ? setWeekDays([...weekDays.filter((i) => i !== day)]) : setWeekDays([...weekDays, day]);
+  };
+
+  return (
+    <Stack>
+      <Typography variant="h6" py={1}>
+        {t(`settings.time`)}
+      </Typography>
+      <Grid component={FormGroup} container>
+        {week.map((day, i) => (
+          <Grid size="auto" key={i}>
+            <FormControlLabel
+              control={<Checkbox checked={weekDays.includes(i)} onChange={() => toggleDay(i)} />}
+              label={t(`week.${day}`)}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Grid container spacing={1}>
+          <Grid size="auto" pt={1}>
+            <TimePicker label={t(`settings.timeStart`)} defaultValue={dayjs('2022-04-17T08:00')} />
+          </Grid>
+          <Grid size="auto" pt={1}>
+            <TimePicker label={t(`settings.timeEnd`)} defaultValue={dayjs('2022-04-17T118:00')} />
+          </Grid>
+        </Grid>
+      </LocalizationProvider>
+      <Typography variant="h6" pt={5}>
+        {t(`settings.login`)}
+      </Typography>
+      <Grid container spacing={1}>
+        <Grid size="auto">
+          <FormControlLabel control={<Switch />} label={t(`settings.vacation`)} />
+        </Grid>
+        <Grid size="auto">
+          <FormControlLabel control={<Switch />} label={t(`settings.oAuth`)} />
+        </Grid>
+        <Grid size="auto">
+          <FormControlLabel control={<Switch />} label={t(`settings.registration`)} />
+        </Grid>
+      </Grid>
+      <Stack direction="row">
+        <Button color="error" sx={{ ml: 'auto', mr: 2 }}>
+          {t('generics.cancel')}
+        </Button>
+        <Button type="submit" variant="contained">
+          {t('generics.confirm')}
+        </Button>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default SysytemSettings;
