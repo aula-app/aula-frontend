a603e0e1ea8b6b6dde8ac076bf15a310fd6f8c19
Eduardo Liron <eduardoliron@gmail.com>
2024-09-03 18:45:18 -0300

    feat(SettingsView): view suspended options

--- src/views/Settings/SettingsView/DataTable.tsx
+++ src/views/Settings/SettingsView/DataTable.tsx
@@ -2,13 +2,14 @@ import { PossibleFields } from '@/types/Scopes';
 import { SettingNamesType } from '@/types/SettingsTypes';
 import { dataSettings } from '@/utils';
 import { Checkbox, Stack, Table, TableBody, TableCell, TableHead, TableRow, TableSortLabel } from '@mui/material';
-import { grey } from '@mui/material/colors';
+import { blueGrey, cyan, grey } from '@mui/material/colors';
 import { Dispatch, Fragment, SetStateAction, useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 
 type Params = {
   handleOrder: (col: number) => void;
-  items: PossibleFields[];
+  activeItems: PossibleFields[];
+  suspendedItems: PossibleFields[];
   orderAsc: boolean;
   orderBy: number;
   scope: SettingNamesType;
@@ -25,7 +26,8 @@ type Params = {
 
 const DataTable = ({
   handleOrder,
-  items,
+  activeItems,
+  suspendedItems,
   orderAsc,
   orderBy,
   scope,
@@ -48,8 +50,8 @@ const DataTable = ({
   };
 
   const toggleAllRows = () => {
-    if (items.length === 0) return;
-    selected.length > 0 ? setSelected([]) : setSelected(items.map((item) => item.id));
+    if (activeItems.length === 0) return;
+    selected.length > 0 ? setSelected([]) : setSelected(activeItems.map((item) => item.id));
   };
 
   useEffect(() => {
@@ -59,7 +61,7 @@ const DataTable = ({
     };
   }, []);
 
-  useEffect(getLimit, [items.length]);
+  useEffect(getLimit, [activeItems.length]);
 
   return (
     <Stack flex={1} sx={{ overflowX: 'auto' }} ref={tableBody}>
@@ -69,8 +71,8 @@ const DataTable = ({
             <TableCell>
               <Checkbox
                 onChange={toggleAllRows}
-                checked={selected.length === items.length}
-                indeterminate={selected.length > 0 && selected.length < items.length}
+                checked={selected.length === activeItems.length}
+                indeterminate={selected.length > 0 && selected.length < activeItems.length}
                 color="secondary"
               />
             </TableCell>
@@ -92,33 +94,81 @@ const DataTable = ({
           </TableRow>
         </TableHead>
         <TableBody>
-          {items.map((row) => (
-            <TableRow
+          {activeItems.map((row) => (
+            <DataRow
               key={row.id}
-              hover
-              sx={{ background: selected.includes(row.id) ? grey[200] : '', cursor: 'pointer' }}
-            >
-              <TableCell>
-                <Checkbox checked={selected.includes(row.id)} onChange={() => toggleRow(row.id)} />
-              </TableCell>
-              {dataSettings[scope].map((column) => (
-                <Fragment key={`${column.name}-${row.id}`}>
-                  {column.orderId >= 0 && (
-                    <TableCell
-                      sx={{ overflow: 'clip', textOverflow: 'ellipsis', whiteSpace: 'nowrap', maxWidth: 500 }}
-                      onClick={() => setAlter({ open: true, id: row.id })}
-                    >
-                      {row[column.name]}
-                    </TableCell>
-                  )}
-                </Fragment>
-              ))}
-            </TableRow>
+              row={row}
+              scope={scope}
+              selected={selected}
+              toggleRow={toggleRow}
+              setAlter={setAlter}
+            />
+          ))}
+          {suspendedItems.map((row) => (
+            <DataRow
+              key={`s${row.id}`}
+              row={row}
+              scope={scope}
+              selected={selected}
+              toggleRow={toggleRow}
+              setAlter={setAlter}
+              suspended
+            />
           ))}
         </TableBody>
       </Table>
     </Stack>
   );
 };
+
+function DataRow({
+  row,
+  scope,
+  selected,
+  toggleRow,
+  setAlter,
+  suspended = false,
+}: {
+  row: PossibleFields;
+  scope: SettingNamesType;
+  selected: number[];
+  toggleRow: (id: number) => void;
+  setAlter: ({ open, id }: { open: boolean; id: number }) => void;
+  suspended?: boolean;
+}) {
+  const background = suspended
+    ? selected.includes(row.id)
+      ? blueGrey[100]
+      : blueGrey[50]
+    : selected.includes(row.id)
+      ? grey[100]
+      : '';
+  return (
+    <TableRow
+      hover
+      sx={{
+        background: background,
+        cursor: 'pointer',
+        textDecorationLine: suspended ? 'line-through' : 'none',
+      }}
+    >
+      <TableCell>
+        <Checkbox checked={selected.includes(row.id)} onChange={() => toggleRow(row.id)} />
+      </TableCell>
+      {dataSettings[scope].map((column) => (
+        <Fragment key={`${column.name}-${row.id}`}>
+          {column.orderId >= 0 && (
+            <TableCell
+              sx={{ overflow: 'clip', textOverflow: 'ellipsis', whiteSpace: 'nowrap', maxWidth: 500 }}
+              onClick={() => setAlter({ open: true, id: row.id })}
+            >
+              {row[column.name]}
+            </TableCell>
+          )}
+        </Fragment>
+      ))}
+    </TableRow>
+  );
+}
 
 export default DataTable;
