31520248b7098f89d9e77886fa48d100f064c6b3
Eduardo Liron <eduardoliron@gmail.com>
2024-09-16 12:39:29 -0300

    feat(requests): end applying error handling on databaseRequest function

---
+++ src/utils/requests.ts
@@ -0,0 +1,51 @@
+import { ObjectPropByName } from '@/types/Generics';
+import { useTranslation } from 'react-i18next';
+import { parseJwt } from './jwt';
+import { localStorageGet } from './localStorage';
+
+interface RequestObject {
+  model: string;
+  method: string;
+  arguments: ObjectPropByName;
+}
+
+export const databaseRequest = async (requestData: RequestObject, userId = [] as string[]) => {
+  const jwt_token = localStorageGet('token');
+  const jwt_payload = parseJwt(jwt_token);
+  const headers = {} as { 'Content-Type'?: string; Authorization?: string };
+  headers['Content-Type'] = 'application/json';
+
+  const error = new CustomEvent('AppErrorDialog', {
+    detail: 'texts.error',
+  });
+
+  if (requestData.method !== 'checkLogin') {
+    headers['Authorization'] = `Bearer ${jwt_token}`;
+  }
+
+  if (userId.length > 0) {
+    userId.forEach((field) => {
+      requestData.arguments[field] = jwt_payload.user_id;
+    });
+  }
+
+  try {
+    const response = await (
+      await fetch(`${import.meta.env.VITE_APP_API_URL}/api/controllers/model.php`, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(requestData),
+      })
+    ).json();
+
+    if (response && response.success) {
+      return response;
+    } else {
+      document.dispatchEvent(error);
+      return response;
+    }
+  } catch (e) {
+    document.dispatchEvent(error);
+    return { success: false };
+  }
+};
