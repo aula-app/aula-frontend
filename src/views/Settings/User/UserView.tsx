4d9fee2de88ad4d7950fb7f3d12a48cca794fecc
Edgar Zanella Alvarenga <e@vaz.io>
2024-09-23 22:20:07 +0200

    Use API URL from multi instances server

--- src/views/Settings/User/UserView.tsx
+++ src/views/Settings/User/UserView.tsx
@@ -30,6 +30,7 @@ import ProfileEditor from './ProfileEditor';
 const UserView = () => {
   const { t } = useTranslation();
   const jwt_token = localStorageGet('token');
+  const api_url = localStorageGet('api_url');
   const [user, setUser] = useState<UserType | null>(null);
   const [openDelete, setOpenDelete] = useState(false);
   const [, dispatch] = useAppStore();
@@ -54,7 +55,7 @@ const UserView = () => {
 
   const setPass = async (oldPass: string, newPass: string) => {
     const request = await (
-      await fetch(`${import.meta.env.VITE_APP_API_URL}/api/controllers/change_password.php`, {
+      await fetch(`${api_url}/api/controllers/change_password.php`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
