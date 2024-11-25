7c6e4a9191d5c831e0cb6f81a592ccb1a9b7e59c
Eduardo Liron <eduardoliron@gmail.com>
2024-11-25 12:55:44 -0300

    feat(jwt.ts): add code documentation

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,15 +1,15 @@
 import { format } from 'date-fns';
 
-export const FORMAT_DATE_TIME = 'YYYY-MM-DD HH:mm:ss';
-export const FORMAT_DATE_ONLY = 'YYYY-MM-DD';
-export const FORMAT_TIME_ONLY = 'HH:mm:ss';
-
 /**
  * Main Data and Time conversion utility to keep formats the same across entire Application
  * @param {string|object} dateOrString - date to show as UTC string or Date object instance
  * @param {string} [dateFormat] - time conversion template in 'date-fns' format, `FORMAT_DATE_TIME` by default
  * @param {string} [fallbackValue] - optional fallback value if data conversion is not possible
  */
+
+export const FORMAT_DATE_TIME = 'YYYY-MM-DD HH:mm:ss';
+export const FORMAT_DATE_ONLY = 'YYYY-MM-DD';
+export const FORMAT_TIME_ONLY = 'HH:mm:ss';
 
 export function dateToString(dateOrString: string | Date, dateFormat = FORMAT_DATE_TIME, fallbackValue = ''): string {
   const date = typeof dateOrString === 'object' ? dateOrString : new Date(dateOrString);
