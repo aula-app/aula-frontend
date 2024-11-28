1122702fd7ba5276af45aac5b9fce7bb7074b864
Eduardo Liron <eduardoliron@gmail.com>
2024-11-28 09:23:20 -0300

    add missing files

---
+++ src/components/SelectRoom/SelectRoom.tsx
@@ -0,0 +1,59 @@
+import { RoomType } from '@/types/Scopes';
+import { databaseRequest } from '@/utils';
+import { MenuItem, TextField } from '@mui/material';
+import { ChangeEvent, ChangeEventHandler, useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+type Params = {
+  room: number | '';
+  setRoom: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
+};
+
+const SelectRoom = ({ room, setRoom }: Params) => {
+  const { t } = useTranslation();
+  const [options, setOptions] = useState<Array<RoomType>>([]);
+
+  const getOptions = async () => {
+    await databaseRequest({
+      model: 'Room',
+      method: 'getRooms',
+      arguments: {
+        offset: 0,
+        limit: 0,
+      },
+    }).then((response) => {
+      if (!response.success) return;
+      setOptions(response.data);
+    });
+  };
+
+  const changeRoom = (event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+    setRoom(event);
+  };
+
+  useEffect(() => {
+    getOptions();
+  }, []);
+
+  return (
+    <TextField
+      select
+      label={t('views.room')}
+      value={room}
+      onChange={changeRoom}
+      variant="filled"
+      size="small"
+      sx={{ minWidth: 200 }}
+      disabled={!options}
+    >
+      <MenuItem value="">&nbsp;</MenuItem>
+      {options.map((option) => (
+        <MenuItem value={option.id} key={option.id}>
+          {option.room_name}
+        </MenuItem>
+      ))}
+    </TextField>
+  );
+};
+
+export default SelectRoom;
