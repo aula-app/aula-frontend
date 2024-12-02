465d834508d8198f2431f6c37a3d6a62b33e8d98
Eduardo Liron <eduardoliron@gmail.com>
2024-12-02 10:47:44 -0300

    add csv listing and confirm button

--- src/views/Settings/Config/UsersSettings/UsersSettings.tsx
+++ src/views/Settings/Config/UsersSettings/UsersSettings.tsx
@@ -2,7 +2,7 @@ import { AppIcon } from '@/components';
 import SelectRoom from '@/components/SelectRoom';
 import { useAppStore } from '@/store';
 import { databaseRequest } from '@/utils';
-import { Button, FormHelperText, Stack } from '@mui/material';
+import { Button, FormHelperText, Stack, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
 import { ChangeEvent, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
@@ -16,6 +16,7 @@ interface Props {
 const DataSettings = ({ onReload }: Props) => {
   const { t } = useTranslation();
   const [, dispatch] = useAppStore();
+  const [users, setUsers] = useState<Array<string>>([]);
   const [room, setRoom] = useState<number | ''>('');
   const [error, setError] = useState<string>('');
 
@@ -23,6 +24,18 @@ const DataSettings = ({ onReload }: Props) => {
     if (!e.target.files || e.target.files.length < 1) return;
 
     Array.from(e.target.files).forEach((file) => readCSV(file));
+  };
+
+  const onSubmit = () => {
+    if (room === '') {
+      setError(t('texts.CSVnoRoom'));
+      return;
+    }
+    if (users.length === 0) {
+      setError(t('texts.CSVempty'));
+      return;
+    }
+    uploadCSV(users.join('/n'));
   };
 
   const readCSV = (file: File) => {
@@ -43,7 +56,7 @@ const DataSettings = ({ onReload }: Props) => {
         return;
       }
       lines.splice(0, 1);
-      uploadCSV(lines.join('\n'));
+      setUsers(lines);
     };
     reader.readAsText(file);
   };
@@ -53,10 +66,6 @@ const DataSettings = ({ onReload }: Props) => {
   };
 
   const uploadCSV = async (csv: string) => {
-    if (room === '') {
-      setError(t('texts.CSVnoRoom'));
-      return;
-    }
     await databaseRequest({
       model: 'User',
       method: 'addCSV',
@@ -70,32 +79,60 @@ const DataSettings = ({ onReload }: Props) => {
         return;
       }
       dispatch({ type: 'ADD_POPUP', message: { message: t('texts.CSVsuccess'), type: 'success' } });
+      setUsers([]);
+      setRoom('');
       onReload();
     });
   };
 
   return (
-    <Stack>
+    <Stack gap={2}>
       <Stack direction="row" alignItems="center" gap={3} flexWrap="wrap">
-        <Stack direction="row" alignItems="center" gap={1}>
-          {t('texts.fileDownload')}:{' '}
-          <a href="/data/aula_users.csv" download target="_blank" rel="noreferrer">
-            <Button color="secondary" variant="outlined">
-              <AppIcon icon="download" size="small" sx={{ mr: 1, ml: -0.5 }} />
-              aula_users.csv
-            </Button>
-          </a>
-        </Stack>
-        <Stack direction="row" alignItems="center" gap={1}>
+        {t('texts.fileDownload')}:{' '}
+        <a href="/data/aula_users.csv" download target="_blank" rel="noreferrer">
+          <Button color="secondary" variant="outlined">
+            <AppIcon icon="download" size="small" sx={{ mr: 1, ml: -0.5 }} />
+            aula_users.csv
+          </Button>
+        </a>
+      </Stack>
+      <Stack>
+        <Stack direction="row" alignItems="center" gap={3}>
+          <Button variant="contained" component="label" color="secondary">
+            {t('texts.fileUpload')}
+            <input type="file" name="my_files" multiple hidden onChange={handleFileChange} />
+          </Button>
           {t('texts.select', { var: t('views.room') })}:
           <SelectRoom room={room} setRoom={selectRoom} />
         </Stack>
-        <Button variant="contained" component="label">
-          {t('texts.fileUpload')}
-          <input type="file" name="my_files" multiple hidden onChange={handleFileChange} />
-        </Button>
+        <FormHelperText error={error !== ''}>{error}</FormHelperText>
       </Stack>
-      <FormHelperText error={error !== ''}>{error}</FormHelperText>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>{t('settings.realname')}</TableCell>
+            <TableCell>{t('settings.displayname')}</TableCell>
+            <TableCell>{t('settings.username')}</TableCell>
+            <TableCell>{t('settings.email')}</TableCell>
+            <TableCell>{t('settings.about_me')}</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map((user) => {
+            const keys = user.split(';');
+            return (
+              <TableRow>
+                {keys.map((key) => (
+                  <TableCell>{key}</TableCell>
+                ))}
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+      <Button variant="contained" component="label" onClick={onSubmit}>
+        {t('generics.confirm')}
+      </Button>
     </Stack>
   );
 };
