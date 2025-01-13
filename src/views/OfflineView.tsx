7cfe1fce2091ec0d7651ca7c0650232eda3b20a2
Eduardo Liron <eduardoliron@gmail.com>
2025-01-13 09:26:19 -0300

    review(OfflineView)

--- src/views/OfflineView.tsx
+++ src/views/OfflineView.tsx
@@ -1,20 +1,25 @@
-import { useEventLogout } from '@/hooks';
 import { Button, Stack, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import { logout } from '@/services/login';
+
+const OFFLINE_IMAGE = '/img/aula_happy.png';
+
+interface OfflineViewProps {
+  className?: string;
+}
 
 /**
- * "OfflineView"
+ * View displayed when application is offline
  */
-const OfflineView = () => {
+const OfflineView: React.FC<OfflineViewProps> = () => {
   const { t } = useTranslation();
-  const onLogout = useEventLogout();
 
   return (
     <Stack height="100vh" alignItems="center" justifyContent="center" p={4}>
-      <Button color="error" sx={{ position: 'absolute', top: 10, left: 10 }} onClick={onLogout}>
+      <Button color="error" sx={{ position: 'absolute', top: 10, left: 10 }} onClick={logout}>
         &lt; {t('login.exit')}
       </Button>
-      <img src="/img/aula_happy.png" alt="Not found" loading="lazy" width={150} />
+      <img src={OFFLINE_IMAGE} alt="Offline status" loading="lazy" width={150} />
       <Typography variant="h6" mt={6} textAlign="center">
         {t('views.offline')}
       </Typography>
