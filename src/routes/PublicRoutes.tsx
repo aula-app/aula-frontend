736b754724fcbb7ab222409497d208c52f0d11da
Eduardo Liron <eduardoliron@gmail.com>
2025-01-13 08:18:23 -0300

    review(PublicRoutes): inprove code and review imports

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,44 +1,45 @@
-import { localStorageGet, localStorageSet } from '@/utils';
-import { NotFoundView } from '@/views/Public';
-import AuthView from '@/views/Public/Auth';
-import LoginRoutes from '@/views/Public/Login';
-import OAuthLogin from '@/views/Public/OAuthLogin';
-import RecoveryRoutes from '@/views/Public/Recovery';
-import InstanceCodeView from '@/views/Public/InstanceCodeView';
-import SetPasswordView from '@/views/Public/SetPassword';
-import ChangePasswordView from '@/views/UpdatePassword';
-
 import { useEffect } from 'react';
 import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+
+import { localStorageGet, localStorageSet } from '@/utils';
+import { NotFoundView, Auth, Login, OAuthLogin, Recovery, InstanceCodeView, SetPassword } from '@/views/Public';
+import ChangePasswordView from '@/views/UpdatePassword';
+
+const useInstanceCheck = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const instance_token = localStorageGet('code', false);
+    const isMultiInstance = import.meta.env.VITE_APP_MULTI !== 'false';
+
+    if (isMultiInstance) {
+      if (!instance_token && location.pathname !== '/code') {
+        navigate('/code');
+      }
+    } else {
+      localStorageSet('code', 'SINGLE');
+      localStorageSet('api_url', import.meta.env.VITE_APP_API_URL);
+    }
+  }, [location.pathname, navigate]);
+};
 
 /**
  * List of routes available only for anonymous users
  */
 const PublicRoutes = () => {
-  let navigate = useNavigate();
-  let location = useLocation();
-
-  useEffect(() => {
-    const instance_token = localStorageGet('code', false);
-
-    if (import.meta.env.VITE_APP_MULTI != 'false') {
-      if (!instance_token && location.pathname != '/code') navigate('/code');
-    } else {
-      localStorageSet('code', 'SINGLE');
-      localStorageSet('api_url', import.meta.env.VITE_APP_API_URL);
-    }
-  });
+  useInstanceCheck();
 
   return (
     <Routes>
-      <Route path="/" element={<LoginRoutes />} />
+      <Route path="/" element={<Login />} />
       <Route path="/code" element={<InstanceCodeView />} />
-      <Route path="login/*" element={<LoginRoutes />} />
+      <Route path="login/*" element={<Login />} />
       <Route path="oauth-login/:jwt_token" element={<OAuthLogin />} />
-      <Route path="recovery/*" element={<RecoveryRoutes />} />
+      <Route path="recovery/*" element={<Recovery />} />
       <Route path="password/" element={<ChangePasswordView />} />
-      <Route path="password/:key" element={<SetPasswordView />} />
-      <Route path="auth" element={<AuthView />} />
+      <Route path="password/:key" element={<SetPassword />} />
+      <Route path="auth" element={<Auth />} />
       <Route path="*" element={<NotFoundView />} />
     </Routes>
   );
--- src/views/Public/index.tsx
+++ src/views/Public/index.tsx
@@ -1,3 +1,19 @@
 import NotFoundView from './PublicNotFoundView';
+import InstanceCodeView from './InstanceCodeView'
+import PublicNotFoundView from './PublicNotFoundView'
+import Auth from './Auth/'
+import Login from './Login/'
+import OAuthLogin from './OAuthLogin/'
+import Recovery from './Recovery/'
+import SetPassword from './SetPassword/'
 
-export { NotFoundView };
+export {
+  NotFoundView,
+  InstanceCodeView,
+  PublicNotFoundView,
+  Auth,
+  Login,
+  OAuthLogin,
+  Recovery,
+  SetPassword,
+};
