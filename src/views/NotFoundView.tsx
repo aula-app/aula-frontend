7f08b2b8f085b0884e509b5db3fa56da965e6e44
Eduardo Liron <eduardoliron@gmail.com>
2025-01-13 09:23:32 -0300

    review(NotFoundView)

--- src/views/NotFoundView.tsx
+++ src/views/NotFoundView.tsx
@@ -2,9 +2,10 @@ import { Stack, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
 /**
- * "Not Found" aka "Error 404" view
+ * "Not Found" aka "Error 404" view component
+ * @returns React component displaying 404 error page
  */
-const NotFoundViewView = () => {
+const NotFoundView = (): JSX.Element => {
   const { t } = useTranslation();
   return (
     <Stack flex={1} alignItems="center" justifyContent="center">
@@ -16,4 +17,4 @@ const NotFoundViewView = () => {
   );
 };
 
-export default NotFoundViewView;
+export default NotFoundView;
