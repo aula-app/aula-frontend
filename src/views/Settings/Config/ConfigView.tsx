80c4864c6a7564b78eb591be62b822afde136cad
Eduardo Liron <eduardoliron@gmail.com>
2025-01-20 09:16:12 -0300

    set(ConfigView): give full access to level 50

--- src/views/Settings/Config/ConfigView.tsx
+++ src/views/Settings/Config/ConfigView.tsx
@@ -3,7 +3,7 @@ import { ConfigResponse, InstanceResponse } from '@/types/Generics';
 import { checkPermissions, databaseRequest } from '@/utils';
 import { Accordion, AccordionDetails, AccordionSummary, Skeleton, Stack, Typography } from '@mui/material';
 import { red } from '@mui/material/colors';
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Categories from './Categories';
 import LoginSettings from './LoginSettings';
@@ -71,11 +71,11 @@ const ConfigView = () => {
     { name: 'group', role: 50, component: <Groups /> },
     { name: 'idea', role: 50, component: <IdeaSettings onReload={getConfig} /> },
     { name: 'user', role: 50, component: <UsersSettings onReload={closePanels} /> },
-    { name: 'time', role: 60, component: <TimeSettings config={config} onReload={getConfig} /> },
-    { name: 'login', role: 60, component: <LoginSettings config={config} settings={settings} onReload={loadData} /> },
-    { name: 'action', role: 60, component: <TimedCommands /> },
-    { name: 'system', role: 60, component: <SystemSettings settings={settings} onReload={getSettings} /> },
-    { name: 'danger', role: 60, component: <SchoolDelete /> },
+    { name: 'time', role: 50, component: <TimeSettings config={config} onReload={getConfig} /> },
+    { name: 'login', role: 50, component: <LoginSettings config={config} settings={settings} onReload={loadData} /> },
+    { name: 'action', role: 50, component: <TimedCommands /> },
+    { name: 'system', role: 50, component: <SystemSettings settings={settings} onReload={getSettings} /> },
+    { name: 'danger', role: 50, component: <SchoolDelete /> },
   ];
 
   const panels = SETTINGS_PANELS.filter((panel) => checkPermissions(panel.role));
@@ -87,9 +87,9 @@ const ConfigView = () => {
       </Typography>
       <SchoolInfo config={config} onReload={getConfig} />
       {panels.map((panel, i) => (
-        <>
+        <Fragment key={i}>
           {settings && config ? (
-            <Accordion expanded={expanded === `panel${i}`} onChange={() => toggleExpanded(`panel${i}`)} key={i}>
+            <Accordion expanded={expanded === `panel${i}`} onChange={() => toggleExpanded(`panel${i}`)}>
               <AccordionSummary expandIcon={<AppIcon icon="arrowdown" />}>
                 <Typography variant="h5" py={1}>
                   {t('settings.advanced.system', { var: t(`settings.panels.${panel.name}`) })}
@@ -98,9 +98,9 @@ const ConfigView = () => {
               <AccordionDetails>{panel.component}</AccordionDetails>
             </Accordion>
           ) : (
-            <Skeleton key={i} variant="rectangular" width="100%" height={45} sx={{ mt: 3 }} />
+            <Skeleton variant="rectangular" width="100%" height={45} sx={{ mt: 3 }} />
           )}
-        </>
+        </Fragment>
       ))}
     </Stack>
   );
