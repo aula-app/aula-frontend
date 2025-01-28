e577d668a3baf507a0f5b09170de0d2c325bcc8d
Eduardo Liron <eduardoliron@gmail.com>
2025-01-28 17:12:49 -0300

    review(BoxCard): updateBoxCard. WIP: MoreOptions

--- src/components/BoxCard/BoxCard.tsx
+++ src/components/BoxCard/BoxCard.tsx
@@ -2,122 +2,88 @@ import AppIcon from '@/components/AppIcon';
 import AppLink from '@/components/AppLink';
 import MoreOptions from '@/components/MoreOptions';
 import { BoxType } from '@/types/Scopes';
-import { checkPermissions, databaseRequest, phases } from '@/utils';
+import { checkPermissions, phases } from '@/utils';
 import { Box, Card, CardContent, Stack, Typography } from '@mui/material';
-import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import BoxCardSkeleton from './BoxCardSkeleton';
 
 interface BoxCardProps {
-  box: string;
+  box: BoxType;
   noLink?: boolean;
-  onReload?: () => void;
+  onReload: () => void;
 }
 
 const BoxCard = ({ box, noLink = false, onReload }: BoxCardProps) => {
   const { t } = useTranslation();
 
-  const [boxData, setBox] = useState<BoxType>();
-  const [remaining, setRemaining] = useState(0);
-
-  const boxFetch = async () =>
-    await databaseRequest({
-      model: 'Topic',
-      method: 'getTopicBaseData',
-      arguments: { topic_id: box },
-    }).then((response) => {
-      if (response.success) setBox(response.data);
-    });
+  // const [remaining, setRemaining] = useState(0);
 
   const daysRemaining = (): number => {
-    if (!boxData) return 0;
     let remaining = 0;
-    for (let i = 0; i < Number(boxData.phase_id) / 10 && i < 3; i++) {
+    for (let i = 0; i < Number(box.phase_id) / 10 && i < 3; i++) {
       const phase = `phase_duration_${i + 1}` as
         | 'phase_duration_1'
         | 'phase_duration_2'
         | 'phase_duration_3'
         | 'phase_duration_4';
-      remaining += Number(boxData[phase]);
+      remaining += Number(box[phase]);
     }
     const currentDate = new Date();
-    const endDate = new Date(boxData.created);
+    const endDate = new Date(box.created);
     endDate.setDate(endDate.getDate() + remaining);
     return Math.round((Number(endDate) - Number(currentDate)) / 86400000);
   };
 
-  const reload = () => {
-    if (onReload) onReload();
-    boxFetch();
-  };
-
-  useEffect(() => {
-    setRemaining(daysRemaining());
-  }, [boxData]);
-
-  useEffect(() => {
-    boxFetch();
-  }, []);
-
-  return boxData ? (
+  return (
     <Card sx={{ borderRadius: '25px', scrollSnapAlign: 'center' }} variant="outlined">
       <Stack
         width="100%"
         height="3rem"
         alignItems="center"
         direction="row"
-        bgcolor={`${phases[boxData.phase_id]}.main`}
+        bgcolor={`${phases[box.phase_id]}.main`}
         p={1}
         pr={2}
       >
-        <AppIcon icon={phases[boxData.phase_id]} sx={{ mx: 1 }} />
+        <AppIcon icon={phases[box.phase_id]} sx={{ mx: 1 }} />
         <Typography variant="caption" mr="auto">
-          {t('ideas.box.toPhase', { var: boxData.ideas_num, phase: t(`phases.${phases[boxData.phase_id]}`) })}
+          {t('patterns.on', {
+            a: t('delegation.status.undelegated', { var: box.ideas_num }),
+            b: t('phases.phase', { var: t(`phases.${phases[box.phase_id]}`) }).toLowerCase(),
+          })}
         </Typography>
-        <MoreOptions scope="boxes" item={boxData} onClose={reload} canEdit={checkPermissions(30)} />
+        {/* <MoreOptions scope="boxes" item={box} onClose={onReload} canEdit={checkPermissions(30)} /> */}
       </Stack>
       <AppLink
-        to={`/room/${boxData.room_hash_id}/phase/${boxData.phase_id}/idea-box/${boxData.hash_id}`}
+        to={`/room/${box.room_hash_id}/phase/${box.phase_id}/idea-box/${box.hash_id}`}
         mb={2}
-        key={boxData.hash_id}
+        key={box.hash_id}
         disabled={noLink}
       >
         <CardContent>
           <Typography variant="h6" noWrap>
-            {boxData.name}
+            {box.name}
           </Typography>
-          <Typography variant="body2">{boxData.description_public}</Typography>
+          <Typography variant="body2">{box.description_public}</Typography>
           <Box
             mt={2}
             position="relative"
             borderRadius={999}
             width="100%"
             height="1.5rem"
-            bgcolor={`${phases[boxData.phase_id]}.main`}
+            bgcolor={`${phases[box.phase_id]}.main`}
             overflow="clip"
           >
-            <Box bgcolor={`${phases[boxData.phase_id]}.main`} position="absolute" left={0} height="100%" width="50%" />
-            <Stack
-              direction="row"
-              position="absolute"
-              left={0}
-              height="100%"
-              width="100%"
-              alignItems="center"
-              justifyContent="end"
-              px={2}
-            >
+            <Box bgcolor={`${phases[box.phase_id]}.main`} position="absolute" left={0} height="100%" width="50%" />
+            <Stack direction="row" position="absolute" left={0} height="100%" width="100%" alignItems="center">
               <AppIcon icon="clock" size="small" sx={{ mx: 0.5 }} />
               <Typography variant="caption">
-                {remaining > 0 ? t('phases.status.end', { var: remaining }) : t('phases.status.ended')}
+                {daysRemaining() > 0 ? t('phases.status.end', { var: daysRemaining() }) : t('phases.status.ended')}
               </Typography>
             </Stack>
           </Box>
         </CardContent>
       </AppLink>
     </Card>
-  ) : (
-    <BoxCardSkeleton />
   );
 };
 
--- src/views/Room/RoomPhaseView/IdeasBoxes/IdeasBoxesView.tsx
+++ src/views/Room/RoomPhaseView/IdeasBoxes/IdeasBoxesView.tsx
@@ -1,64 +1,57 @@
 import { AppIcon } from '@/components';
 import BoxCard from '@/components/BoxCard';
+import BoxCardSkeleton from '@/components/BoxCard/BoxCardSkeleton';
 import EditData from '@/components/Data/EditData';
+import { getBoxesByPhase } from '@/services/boxes';
 import { BoxType } from '@/types/Scopes';
 import { checkPermissions, databaseRequest } from '@/utils';
-import { Fab, Stack } from '@mui/material';
+import { Fab, Stack, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid2';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useNavigate, useParams } from 'react-router-dom';
 
 /** * Renders "IdeasBox" view
  * url: /room/:room_id/:phase
  */
 const IdeasBoxView = () => {
-  const goto = useNavigate();
-  const params = useParams();
-  const [addBox, setAddBox] = useState(false);
-  const [addSurvey, setAddSurvey] = useState(false);
+  const { t } = useTranslation();
+  const { room_id, phase } = useParams();
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
   const [boxes, setBoxes] = useState<BoxType[]>([]);
 
-  const boxesFetch = async () =>
-    await databaseRequest(
-      {
-        model: 'Topic',
-        method: 'getTopics',
-        arguments: {
-          offset: 0,
-          limit: 0,
-          room_id: params['room_id'],
-          phase_id: Number(params['phase']),
-        },
-      },
-      ['user_id']
-    ).then((response) => {
-      if (!response.success || !response.data) return;
-      setBoxes(response.data as BoxType[]);
-    });
-
-  const closeAdd = () => {
-    boxesFetch();
-    setAddBox(false);
-    setAddSurvey(false);
-  };
+  const fetchBoxes = useCallback(async () => {
+    if (!room_id || !phase) return;
+    setLoading(true);
+    const response = await getBoxesByPhase(Number(phase), room_id);
+    setLoading(false);
+    setError(response.error);
+    if (!response.error && response.data) setBoxes(response.data);
+    setLoading(false);
+  }, [room_id, phase]);
 
   useEffect(() => {
-    setBoxes([]);
-    //@ts-ignore
-    if (params.phase) boxesFetch();
-    else goto('/error');
-  }, [params['phase']]);
+    fetchBoxes();
+  }, []);
 
   return (
     <Stack alignItems="center">
       <Grid container spacing={2} p={1} width="100%">
-        {boxes.map((box) => (
-          <Grid key={box.hash_id} size={{ xs: 12, sm: 6, lg: 4, xl: 3 }} sx={{ scrollSnapAlign: 'center' }}>
-            <BoxCard box={box.hash_id} onReload={boxesFetch} />
+        {isLoading && (
+          <Grid size={{ xs: 12, sm: 6, lg: 4, xl: 3 }} sx={{ scrollSnapAlign: 'center' }}>
+            <BoxCardSkeleton />
           </Grid>
-        ))}
+        )}
+        {error && <Typography>{t(error)}</Typography>}
+        {!isLoading &&
+          boxes.map((box) => (
+            <Grid key={box.hash_id} size={{ xs: 12, sm: 6, lg: 4, xl: 3 }} sx={{ scrollSnapAlign: 'center' }}>
+              <BoxCard box={box} onReload={fetchBoxes} />
+            </Grid>
+          ))}
       </Grid>
-      {checkPermissions(30) && (
+      {/* {checkPermissions(30) && (
         <Stack direction="row" sx={{ position: 'fixed', bottom: 40 }} gap={5}>
           <Fab aria-label="add" color="primary" onClick={() => setAddBox(true)}>
             <AppIcon icon="box" />
@@ -71,7 +64,7 @@ const IdeasBoxView = () => {
           <EditData scope="boxes" isOpen={addBox} otherData={{ room_id: params.room_id }} onClose={closeAdd} />
           <EditData scope="surveys" isOpen={addSurvey} otherData={{ room_id: params.room_id }} onClose={closeAdd} />
         </Stack>
-      )}
+      )} */}
     </Stack>
   );
 };
