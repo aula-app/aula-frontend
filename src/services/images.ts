5e4c7c6488c8baf37c28345f912007f55f1fc87c
Eduardo Liron <eduardoliron@gmail.com>
2025-02-10 13:14:13 -0300

    add missing files

---
+++ src/services/images.ts
@@ -0,0 +1,9 @@
+import { baseRequest } from './requests';
+
+export const uploadImage = async (file: File): Promise<any> => {
+  const formData = new FormData();
+  formData.append('file', file, 'avatar.png');
+  formData.append('fileType', 'avatar');
+
+  return baseRequest('/api/controllers/upload.php', formData);
+};
