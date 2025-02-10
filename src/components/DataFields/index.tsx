05adbb7f6c9b3f50537a1042a987d5e1fb654ddc
Eduardo Liron <eduardoliron@gmail.com>
2025-02-10 17:32:29 -0300

    add room image selector

--- src/components/DataFields/index.tsx
+++ src/components/DataFields/index.tsx
@@ -1,5 +1,4 @@
 import ConsentField from './ConsentFiled';
-import ImageField from './ImageField';
 import MarkdownEditor from './MarkdownEditor';
 import PhaseDurationFields from './PhaseDurationFields';
 import RoleField from './RoleField';
@@ -7,13 +6,4 @@ import SelectField from './SelectField';
 import SelectRoomField from './SelectRoomField';
 import StatusField from './StatusField';
 
-export {
-  ConsentField,
-  ImageField,
-  MarkdownEditor,
-  PhaseDurationFields,
-  RoleField,
-  SelectField,
-  SelectRoomField,
-  StatusField,
-};
+export { ConsentField, MarkdownEditor, PhaseDurationFields, RoleField, SelectField, SelectRoomField, StatusField };
---
+++ src/components/RoomImage/UserAvatar.tsx
@@ -0,0 +1,44 @@
+import { getAvatar } from '@/services/media';
+import { Avatar, AvatarProps } from '@mui/material';
+import { useEffect, useState } from 'react';
+import AppIcon from '../AppIcon';
+
+interface Props extends AvatarProps {
+  id: string;
+  size?: number;
+}
+
+/**
+ * Renders User info with Avatar
+ * @component UserInfo
+ */
+const UserAvatar = ({ id, size = 32, sx, ...restOfProps }: Props) => {
+  const [userAvatar, setUserAvatar] = useState<string>('');
+  const downloadUserAvatar = async () => {
+    const response = await getAvatar(id);
+    if (!response.data) return;
+    if (response.data.length > 0) setUserAvatar(response.data[0].filename);
+  };
+
+  useEffect(() => {
+    downloadUserAvatar();
+  }, [id]);
+
+  return (
+    <Avatar
+      {...restOfProps}
+      sx={{
+        width: size,
+        height: size,
+        fontSize: '3rem',
+        ...sx,
+      }}
+      alt="User avatar"
+      src={`/files/${userAvatar}` || ''}
+    >
+      {!userAvatar && <AppIcon icon="avatar" />}
+    </Avatar>
+  );
+};
+
+export default UserAvatar;
