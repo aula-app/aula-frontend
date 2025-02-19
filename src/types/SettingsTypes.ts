a691a35d704766ad906d354135101f672a6ea94b
Eduardo Liron <eduardoliron@gmail.com>
2025-02-19 13:06:32 -0300

    update permissions

--- src/types/SettingsTypes.ts
+++ src/types/SettingsTypes.ts
@@ -2,6 +2,19 @@ import { PossibleFields } from './Scopes';
 
 export type RoomPhases = 0 | 10 | 20 | 30 | 40;
 export type PhaseType = 'wild' | 'discussion' | 'approval' | 'voting' | 'results';
+
+/*
+ * 10 => "guest",
+ * 20 => "user",
+ * 30 => "moderator",
+ * 31 => "moderator_v",
+ * 40 => "super_moderator",
+ * 41 => "super_moderator_v",
+ * 44 => "principal",
+ * 45 => "principal_v",
+ * 50 => "admin",
+ * 60 => "tech_admin"
+ */
 export type RoleTypes = 10 | 20 | 30 | 31 | 40 | 41 | 44 | 45 | 50 | 60;
 
 export type ScopeNameType = 'idea' | 'room' | 'user';
