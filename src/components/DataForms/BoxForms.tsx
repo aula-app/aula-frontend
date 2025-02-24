02b1bc71535360efd413182871b7938cb0353821
Eduardo Liron <eduardoliron@gmail.com>
2025-02-24 10:02:49 -0300

    feat(BoxFormas): improve phase options using useState

--- src/components/DataForms/BoxForms.tsx
+++ src/components/DataForms/BoxForms.tsx
@@ -33,6 +33,8 @@ const BoxForms: React.FC<BoxFormsProps> = ({ defaultValues, onClose }) => {
   const [updateIdeas, setUpdateIdeas] = useState<UpdateType>({ add: [], remove: [] });
   const [isLoading, setIsLoading] = useState(false);
 
+  const [options, setOptions] = useState<{ label: string; value: number; disabled: boolean }[]>(phaseOptions);
+
   const schema = yup.object({
     name: yup.string().required(t('forms.validation.required')),
     description_public: yup.string().required(t('forms.validation.required')),
@@ -70,16 +72,20 @@ const BoxForms: React.FC<BoxFormsProps> = ({ defaultValues, onClose }) => {
   const validatePhaseTransition = () => {
     let ideasWithApprovalStatus = 0;
     for (let idea of ideas) {
-      if (idea.approved != null) ideasWithApprovalStatus += 1;
+      if (idea.approved) ideasWithApprovalStatus += 1;
     }
 
-    // Prevent moving to Voting phase if there is still ideas waiting for approval
-    phaseOptions[2] = { ...phaseOptions[2], disabled: ideasWithApprovalStatus < ideas.length };
-
-    // A Box can only fo to results if it passed Voting phase
-    if (defaultValues && Number(defaultValues['phase_id']) !== 30)
-      phaseOptions[3] = { ...phaseOptions[3], disabled: true };
-    else phaseOptions[3] = { ...phaseOptions[3], disabled: false };
+    setOptions((prevOptions) =>
+      prevOptions.map((option, index) => {
+        console.log(index, index === 3 && defaultValues && Number(defaultValues['phase_id']) < 30);
+        // Prevent moving to Voting phase if there is still ideas waiting for approval
+        if (index === 2) return { ...option, disabled: ideasWithApprovalStatus < ideas.length };
+        // A Box can only change to results if it passed Voting phase
+        if (index === 3 && defaultValues && Number(defaultValues['phase_id']) < 30)
+          return { ...option, disabled: true };
+        return phaseOptions[index];
+      })
+    );
   };
 
   const onSubmit = async (data: SchemaType) => {
@@ -179,7 +185,7 @@ const BoxForms: React.FC<BoxFormsProps> = ({ defaultValues, onClose }) => {
                 <SelectField
                   control={control}
                   name="phase_id"
-                  options={phaseOptions}
+                  options={options}
                   defaultValue={10}
                   disabled={isLoading}
                 />
