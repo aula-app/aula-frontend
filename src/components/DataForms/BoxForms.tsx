7309ffc2498535c234ed31323a8aa7fdec79f498
Eduardo Liron <eduardoliron@gmail.com>
2025-02-24 10:04:13 -0300

    fix identation

--- src/components/DataForms/BoxForms.tsx
+++ src/components/DataForms/BoxForms.tsx
@@ -78,11 +78,14 @@ const BoxForms: React.FC<BoxFormsProps> = ({ defaultValues, onClose }) => {
     setOptions((prevOptions) =>
       prevOptions.map((option, index) => {
         console.log(index, index === 3 && defaultValues && Number(defaultValues['phase_id']) < 30);
+
         // Prevent moving to Voting phase if there is still ideas waiting for approval
         if (index === 2) return { ...option, disabled: ideasWithApprovalStatus < ideas.length };
+
         // A Box can only change to results if it passed Voting phase
         if (index === 3 && defaultValues && Number(defaultValues['phase_id']) < 30)
           return { ...option, disabled: true };
+
         return phaseOptions[index];
       })
     );
