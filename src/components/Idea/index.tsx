808057e05f54b8994d261a369302daeec12aba32
Eduardo Liron <eduardoliron@gmail.com>
2025-02-26 18:09:31 -0300

    remove unused component

--- src/components/Idea/index.tsx
+++ src/components/Idea/index.tsx
@@ -1,8 +1,7 @@
 import ApprovalCard from './ApprovalCard';
-import SetWinnerCard from './SetWinnerCard';
 import IdeaBubble from './IdeaBubble';
 import IdeaCard from './IdeaCard';
 import VotingCard from './VotingCard';
 import VotingResults from './VotingResults';
 
-export { SetWinnerCard, ApprovalCard, IdeaBubble, IdeaCard, VotingCard, VotingResults };
+export { ApprovalCard, IdeaBubble, IdeaCard, VotingCard, VotingResults };
