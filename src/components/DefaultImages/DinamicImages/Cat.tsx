0cf80ff0e5cc367e03a07d70879300c3b611ab18
Eduardo Liron <eduardoliron@gmail.com>
2025-03-21 08:15:27 -0300

    reove stroke from defaultImages

--- src/components/DefaultImages/DinamicImages/Cat.tsx
+++ src/components/DefaultImages/DinamicImages/Cat.tsx
@@ -7,6 +7,7 @@ export default ({ colors }: { colors: ObjectPropByName }) => {
       version="1.1"
       id="Layer_1"
       xmlns="http://www.w3.org/2000/svg"
+      stroke-width="0"
       x="0px"
       y="0px"
       viewBox="0 0 1024 768"
