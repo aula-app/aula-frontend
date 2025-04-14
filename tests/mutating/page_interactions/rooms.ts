783a66cf29c47bb0f23585130aec59fec052ff90
Niko Niko <work@niko.io>
2025-04-14 13:21:35 +0200

    tmp

---
+++ tests/mutating/page_interactions/rooms.ts
@@ -0,0 +1,32 @@
+import { expect, Page } from '@playwright/test';
+
+import * as shared from '../../shared';
+import * as users from '../../fixtures/users';
+import { sleep } from '../../utils';
+
+const host = shared.getHost();
+
+export const create = async (page: Page, roomName: string, users: users.UserData[]) => {
+  // start at home
+  await page.goto(host);
+
+  await page.locator('a[href="/settings/rooms"]').click();
+
+  await page.getByRole('button', { name: 'Raum hinzufügen' }).click();
+
+  // fill in the necessary information
+  await page.fill('input[name="room_name"]', 'room-' + roomName + '-' + shared.timestamp);
+  await page.locator('div[contenteditable="true"]').fill('generated during automated tests');
+
+  await page.locator('input[role="combobox"]').click();
+
+  await page.getByRole('option', { name: users[0].displayName }).click();
+  await page.locator('input[role="combobox"]').click();
+
+  await page.getByRole('option', { name: users[1].displayName }).click();
+  await page.locator('input[role="combobox"]').click();
+
+  await page.getByRole('option', { name: users[2].displayName }).click();
+
+  await page.locator('button[type="submit"]').click();
+};
--- tests/mutating/users.spec.ts
+++ tests/mutating/users.spec.ts
@@ -2,75 +2,24 @@ import { test, expect, BrowserContext, Page, chromium, Browser } from '@playwrig
 import { sleep } from '../utils';
 import * as shared from '../shared';
 import * as users from './page_interactions/users';
+import * as rooms from './page_interactions/rooms';
 import * as fixtures from '../fixtures/users';
 import * as browsers from './browsers';
 
 const host = shared.getHost();
 
-test.describe('Multi-user test with separate browser contexts', () => {
-  test.skip(({ browserName }) => browserName !== 'chromium', 'Only run in Chromium');
+test('create room', async ({ page }) => {
+  console.log('test creating room!');
+  // Expect a title "to contain" a substring.
 
-  //
-  test.beforeAll('set up browsers', async ({ browser }) => {
-    console.log('setting up browsers...');
+  console.log('hellooo');
+  console.log(browsers.b);
 
-    // Create a context for each user
-    await browsers.init(browser);
-
-    // Log in each user
-    await users.login(browsers.admin, fixtures.admin);
-
-    // Admin should create the new users of varying permission levels
-    console.info('creating alice...');
-    const alicesTempPass = await users.create(browsers.admin, fixtures.alice);
-    console.info('creating bob...');
-    const bobsTempPass = await users.create(browsers.admin, fixtures.bob);
-    console.info('creating mallory...');
-    const mallorysTempPass = await users.create(browsers.admin, fixtures.mallory);
-    console.info('creating burt...');
-    const burtsTempPass = await users.create(browsers.admin, fixtures.burt);
-    console.info('creating rainer...');
-    const rainersTempPass = await users.create(browsers.admin, fixtures.rainer);
-
-    // mallory should _not_ be able to log in with bob's temporary password.
-    await expect(async () => {
-      await users.firstLoginFlow(browsers.mallory, fixtures.mallory, bobsTempPass);
-    }).rejects.toThrow();
-
-    // now we should correctly log in all of the users into their browsers.
-    // first login flow should work nicely!
-    await Promise.all([
-      users.firstLoginFlow(browsers.alice, fixtures.alice, alicesTempPass),
-      users.firstLoginFlow(browsers.bob, fixtures.bob, bobsTempPass),
-      users.firstLoginFlow(browsers.mallory, fixtures.mallory, mallorysTempPass),
-      users.firstLoginFlow(browsers.burt, fixtures.burt, burtsTempPass),
-      users.firstLoginFlow(browsers.rainer, fixtures.rainer, rainersTempPass),
-    ]);
-  });
-
-  test.afterAll(async () => {
-    await users.remove(browsers.admin, fixtures.alice);
-    console.info('deleted alice...');
-    await users.remove(browsers.admin, fixtures.bob);
-    console.info('deleted bob...');
-    await users.remove(browsers.admin, fixtures.mallory);
-    console.info('deleted mallory...');
-    await users.remove(browsers.admin, fixtures.burt);
-    console.info('deleted burt...');
-    await users.remove(browsers.admin, fixtures.rainer);
-    console.info('deleted rainer...');
-
-    // await sleep(5);
-
-    await browsers.shutdown();
-
-    console.info('...done!');
-  });
-
-  test('has title', async ({ page }) => {
-    await browsers.admin.goto(host);
-
-    // Expect a title "to contain" a substring.
-    await expect(browsers.admin).toHaveTitle(/aula/);
-  });
+  await rooms.create(browsers.admin, 'tesssttt', [
+    //
+    fixtures.rainer, //
+    fixtures.alice,
+    fixtures.bob,
+    fixtures.mallory, //
+  ]);
 });
