12e209dc44265d85c4fe948257c5ed3cefbb1d79
Niko Niko <work@niko.io>
2025-04-17 13:43:30 +0200

    pull host from env vars

--- tests/mutating/page_interactions/ideas.ts
+++ tests/mutating/page_interactions/ideas.ts
@@ -23,7 +23,7 @@ export const create = async (
   await RoomDiv.click();
 
   const AddIdeaButton = page.locator('[aria-label="add idea"]');
-  await expect(AddIdeaButton).toBeVisible();
+  await expect(AddIdeaButton).toBeVisible({ timeout: 5000 });
   await AddIdeaButton.click();
 
   // fill in the necessary information
@@ -35,4 +35,30 @@ export const create = async (
 
   const IdeaTitle = page.locator('h3').filter({ hasText: idea.name });
   await expect(IdeaTitle).toBeVisible();
+};
+
+export const comment = async (
+  page: Page, //
+  room: roomFixtures.RoomData,
+  idea: ideaFixtures.IdeaData
+) => {
+  // start at home
+  await page.goto(host);
+
+  const RoomDiv = page.locator('h3').filter({ hasText: room.name });
+  await expect(RoomDiv).toBeVisible();
+  await RoomDiv.click();
+
+  const IdeaDiv = page.locator('h3').filter({ hasText: idea.name });
+  await expect(IdeaDiv).toBeVisible();
+  await IdeaDiv.click();
+
+  const AddCommentButton = page.locator('[aria-label="add comment"]');
+  await expect(AddCommentButton).toBeVisible();
+  await AddCommentButton.click();
+
+  await page.locator('div[contenteditable="true"]').fill('Blah Blah Blah generated in testing');
+
+  // submit the comment form
+  await page.locator('button[type="submit"]').click();
 };
