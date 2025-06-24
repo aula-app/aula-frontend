65e7dd3447ab88b4617f73f48f2fd67f392b8015
Eduardo Liron <eduardoliron@gmail.com>
2025-06-24 14:05:22 -0300

    Remove unused imports

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,7 +2,6 @@ import { AccessibleDialog, AccessibleModal, ConfirmDialog } from './AccessibleDi
 import AppIcon from './AppIcon';
 import AppIconButton from './AppIconButton';
 import AppLink from './AppLink';
-import EmptyState from './EmptyState';
 import ErrorBoundary from './ErrorBoundary';
 import LoadingIndicator from './LoadingIndicator';
 
