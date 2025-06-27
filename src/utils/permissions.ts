ab3080f7ebbf9a0634ca431b177a6432c37c35dd
Eduardo Liron <eduardoliron@gmail.com>
2025-06-27 09:12:30 -0300

    Reorder utils files, to simplify coding experience

--- src/utils/permissions.ts
+++ src/utils/permissions.ts
@@ -2,6 +2,21 @@ import { RoleTypes } from '@/types/SettingsTypes';
 import { parseJwt } from './jwt';
 import { localStorageGet } from './localStorage';
 
+/** User roles:
+ * 10 => "guest",
+ * 20 => "user",
+ * 30 => "moderator",
+ * 31 => "moderator_v",
+ * 40 => "super_moderator",
+ * 41 => "super_moderator_v",
+ * 44 => "principal",
+ * 45 => "principal_v",
+ * 50 => "admin",
+ * 60 => "tech_admin"
+ */
+
+export const roles = [10, 20, 30, 31, 40, 41, 44, 45, 50, 60] as Array<RoleTypes>;
+
 const USER_ROLES = [20, 30, 31, 40, 41, 44, 45];
 const VOTING_ROLES = [20, 31, 41, 45];
 const ADMIN = [50];
