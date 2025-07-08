5444c3c9aa4e894478470d9721c3fe1af66c0952
Eduardo Liron <eduardoliron@gmail.com>
2025-07-08 15:34:22 -0300

    add sorting on wildIdeas view

--- src/views/WildIdeas/WildIdeasView.tsx
+++ src/views/WildIdeas/WildIdeasView.tsx
@@ -11,6 +11,7 @@ import { Drawer, Fab, Stack, Typography } from '@mui/material';
 import { SyntheticEvent, useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
+import SortButton from '@/components/Buttons/SortButton';
 
 interface RouteParams extends Record<string, string | undefined> {
   room_id: string;
@@ -28,10 +29,19 @@ const WildIdeas = () => {
   const { phase, room_id } = useParams<RouteParams>();
   const [appState, dispatch] = useAppStore();
 
+  const IDEAS_SORT_OPTIONS = [
+    { label: 'Creation date', value: 'created' },
+    { label: 'Likes', value: 'sum_likes' },
+    { label: 'Number of comments', value: 'sum_comments' },
+  ] as Array<{ label: string; value: keyof IdeaType }>;
+
   const [isLoading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [ideas, setIdeas] = useState<IdeaType[]>([]);
   const [edit, setEdit] = useState<IdeaType | boolean>(false); // false = update dialog closed ;true = new idea; IdeaType = edit idea;
+
+  const [orderby, setOrderby] = useState<keyof IdeaType>(IDEAS_SORT_OPTIONS[0].value);
+  const [asc, setAsc] = useState(false);
 
   const getRoomName = (id: string) => {
     return getRoom(id).then((response) => {
@@ -111,18 +121,42 @@ const WildIdeas = () => {
     >
       {isLoading && <IdeaBubbleSkeleton />}
       {error && <Typography>{t(error)}</Typography>}
-      {!isLoading && !error && ideas.length === 0 && (
-        <EmptyState title={t('ui.empty.ideas.title')} description={t('ui.empty.ideas.description')} />
-      )}
       {!isLoading &&
-        ideas.map((idea) => (
-          <IdeaBubble
-            key={idea.id}
-            idea={idea}
-            to={`idea/${idea.hash_id}`}
-            onEdit={() => onEdit(idea)}
-            onDelete={() => onDelete(idea.hash_id)}
-          />
+        !error &&
+        (ideas.length === 0 ? (
+          <EmptyState title={t('ui.empty.ideas.title')} description={t('ui.empty.ideas.description')} />
+        ) : (
+          <Stack width="100%">
+            <Stack direction="row" justifyContent="space-between" alignItems="center" width="100%" pb={2}>
+              <Stack direction="row" alignItems="center" gap={1}>
+                <AppIcon icon="idea" />
+                <Typography variant="h2">{t('ui.navigation.ideas')}</Typography>
+              </Stack>
+              <SortButton
+                options={IDEAS_SORT_OPTIONS}
+                onSelect={(orderby: string) => {
+                  console.log('orderby', orderby);
+                  setOrderby(orderby as keyof IdeaType);
+                }}
+                onReorder={(asc: boolean) => {
+                  console.log('asc', asc);
+                  setAsc(asc);
+                }}
+              ></SortButton>
+            </Stack>
+            <Stack width="100%" direction={asc ? 'column-reverse' : 'column'} gap={2}>
+              {ideas.map((idea, creationOrder) => (
+                <IdeaBubble
+                  order={typeof idea[orderby] === 'number' ? -1 * idea[orderby] : creationOrder}
+                  key={idea.id}
+                  idea={idea}
+                  to={`idea/${idea.hash_id}`}
+                  onEdit={() => onEdit(idea)}
+                  onDelete={() => onDelete(idea.hash_id)}
+                />
+              ))}
+            </Stack>
+          </Stack>
         ))}
       {checkPermissions('ideas', 'create') && room_id && (
         <Fab
