418fa6471503179a1c4f8e101435dc55ca8e3a82
Eduardo Liron <eduardoliron@gmail.com>
2025-09-02 10:41:22 -0300

    feat: add frontend filtering to boxes (#811)
    ## Context <!-- ie. explanations, background, documentation -->
    
    Boxes were being filtered on backend, leading to slow response and error
    prone behaviour. It was moved to front end
    
    ## Checklist
    
    - [x] Tested manually <!-- you can strikethrough this option in case you
    haven't tested manually -->
    - [ ] Passed automatic tests <!-- you can strikethrough this option in
    case you haven't run the automatic tests for some reason -->
    - [x] GitHub issue linked <!-- Use the "Development" field of the Issue,
    or add a link if it's outside this Repo -->
    - [ ] Changelist updated
    - [x] Backward and forward compatible with BE <!-- If not, please
    describe in detail and include other PR links -->
    - [x] Doesn't need update in the database or BE <!-- If it does, please
    describe how to deploy it without downtime -->
    - [x] Must be deployed ASAP (HOTFIX)

--- src/components/DataTable/TableSkeleton.tsx
+++ src/components/DataTable/TableSkeleton.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Skeleton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
 interface TableSkeletonProps {
   rows?: number;
@@ -7,14 +8,25 @@ interface TableSkeletonProps {
 }
 
 const TableSkeleton: React.FC<TableSkeletonProps> = ({ rows = 5, columns = 4 }) => {
+  const { t } = useTranslation();
+
   return (
-    <TableContainer component={Paper}>
-      <Table>
+    <TableContainer
+      component={Paper}
+      role="status"
+      aria-label={t('ui.accessibility.loading', { var: t('ui.accessibility.tableData') })}
+    >
+      <Table aria-busy="true">
         <TableHead>
           <TableRow>
             {Array.from({ length: columns }).map((_, index) => (
               <TableCell key={index}>
-                <Skeleton variant="text" width="80%" height={24} />
+                <Skeleton
+                  variant="text"
+                  width="80%"
+                  height={24}
+                  aria-label={t('ui.accessibility.loading', { var: t('ui.accessibility.columnHeader') })}
+                />
               </TableCell>
             ))}
           </TableRow>
@@ -24,7 +36,12 @@ const TableSkeleton: React.FC<TableSkeletonProps> = ({ rows = 5, columns = 4 }) 
             <TableRow key={rowIndex}>
               {Array.from({ length: columns }).map((_, colIndex) => (
                 <TableCell key={colIndex}>
-                  <Skeleton variant="text" width="60%" height={20} />
+                  <Skeleton
+                    variant="text"
+                    width="60%"
+                    height={20}
+                    aria-label={t('ui.accessibility.loading', { var: t('ui.accessibility.tableCell') })}
+                  />
                 </TableCell>
               ))}
             </TableRow>
