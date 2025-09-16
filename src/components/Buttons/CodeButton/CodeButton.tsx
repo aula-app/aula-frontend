ae0164c386bd19a31745ef03220ba6c52d8fdd32
Eduardo Liron <eduardoliron@gmail.com>
2025-09-16 12:08:04 -0300

    add CodeButton component

---
+++ src/components/Buttons/CodeButton/CodeButton.tsx
@@ -0,0 +1,38 @@
+import { useAppStore } from '@/store';
+import { announceToScreenReader, localStorageGet } from '@/utils';
+import { Button, Divider } from '@mui/material';
+import { forwardRef } from 'react';
+import { useTranslation } from 'react-i18next';
+
+const CodeButton = forwardRef<HTMLButtonElement>(({ ...restOfProps }) => {
+  const { t } = useTranslation();
+  const [, dispatch] = useAppStore();
+  const code = localStorageGet('code');
+
+  return (
+    <>
+      <Button
+        onClick={() => {
+          navigator.clipboard.writeText(code).then(() =>
+            dispatch({
+              type: 'ADD_POPUP',
+              message: { message: t('ui.accessibility.codeCopied'), type: 'success' },
+            })
+          );
+          announceToScreenReader(t('ui.accessibility.codeCopied'), 'polite');
+        }}
+        color="secondary"
+        className="app-code"
+        aria-label={t('ui.accessibility.copyInstanceCode', { code })}
+        {...restOfProps}
+      >
+        {`${t('instance.chip')}: ${code}`}
+      </Button>
+      <Divider role="presentation" />
+    </>
+  );
+});
+
+CodeButton.displayName = 'CodeButton';
+
+export default CodeButton;
