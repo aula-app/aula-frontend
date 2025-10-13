2b2701d1a4dbdcddc062b6c8e05601c17391073a
Eduardo Liron <eduardoliron@gmail.com>
2025-10-13 13:57:21 -0300

    add boxes files

---
+++ tests/mutating/main/base-boxes.spec.ts
@@ -0,0 +1,116 @@
+import { test, expect } from '@playwright/test';
+import * as userData from '../../fixtures/users';
+import { describeWithSetup } from '../../shared/base-test';
+import * as entities from '../../shared/helpers/entities';
+import * as browsers from '../../shared/interactions/browsers';
+import * as rooms from '../../shared/interactions/rooms';
+import * as boxes from '../../shared/interactions/boxes';
+import * as ideas from '../../shared/interactions/ideas';
+import * as navigation from '../../shared/interactions/navigation';
+import { BoxData } from '../../fixtures/types';
+
+describeWithSetup('Box Management - Creation, phase changes and Permissions', () => {
+  let admin: any;
+  let user: any;
+  let otherUser: any;
+
+  const room = entities.createRoom('room-tests');
+  const box = entities.createBox('box-in-room', room);
+  const boxIdea = entities.createIdea('box-idea');
+
+  test.beforeAll(async () => {
+    const user1Data = await userData.use('user');
+    const user2Data = await userData.use('other-user');
+
+    admin = await browsers.getUserBrowser('admin');
+    user = await browsers.getUserBrowser(user1Data.username);
+    otherUser = await browsers.getUserBrowser(user2Data.username);
+
+    room.users = [user1Data, user2Data];
+  });
+
+  test.afterAll(async () => {
+    await cleanup();
+  });
+
+  const cleanupQueue = {
+    room: false,
+    box: false,
+  };
+
+  const cleanup = async () => {
+    if (cleanupQueue.box) {
+      cleanupQueue.box = false;
+      await boxes.remove(admin, box);
+    }
+    if (cleanupQueue.room) {
+      cleanupQueue.room = false;
+      await rooms.remove(admin, room);
+    }
+  };
+
+  test('Admin can create a box inside a room', async () => {
+    await rooms.create(admin, room);
+    cleanupQueue.room = true;
+
+    await boxes.create(admin, box); // criar box dentro da sala
+    cleanupQueue.box = true;
+  });
+
+  test('User cannot create a box', async () => {
+    await test.expect(boxes.create(user, entities.createBox('unauthorized-box', room))).rejects.toThrow();
+  });
+
+  test('User can access the new box', async () => {
+    await navigation.goToPhase(user, room.name, 10);
+    await navigation.clickOnPageItem(user, box.name);
+    const boxTitle = user.getByTestId('box-card').getByText(box.name);
+    await expect(boxTitle).toBeVisible();
+  });
+
+  // test('User can create an Idea in the box', async () => {
+  //   await navigation.goToPhase(user, room.name, 10);
+  //   await navigation.clickOnPageItem(user, box.name);
+  //   const boxTitle = user.getByTestId('box-card').getByText(box.name);
+  //   await expect(boxTitle).toBeVisible();
+  //   await ideas.create(user, boxIdea);
+  // });
+
+  test('Admin can move an Idea in the box', async () => {
+    await navigation.goToRoom(user, room.name);
+    await ideas.create(user, boxIdea);
+
+    const boxNewPhaseObject = { ...box, ideas: [boxIdea] } as BoxData;
+
+    await navigation.goToPhase(admin, room.name, 10);
+    await navigation.clickOnPageItem(admin, box.name);
+    const boxCard = admin.getByTestId('box-card');
+    await expect(boxCard.getByText(box.name)).toBeVisible();
+    await boxCard.getByTestId('more-options').click();
+    await admin.waitForTimeout(500);
+    await boxCard.getByTestId('edit-button').click();
+    await boxes.fill(admin, boxNewPhaseObject);
+
+    const boxTitle = admin.getByText(boxIdea.name);
+    await expect(boxTitle).toBeVisible();
+  });
+
+  test('Admin can change box phase', async () => {
+    const boxNewPhaseObject = { ...box, phase: 20 } as BoxData;
+    await boxes.edit(admin, boxNewPhaseObject);
+
+    await navigation.goToPhase(user, room.name, 20);
+    const boxTitle = user.getByTestId('box-card').getByText(box.name);
+    await expect(boxTitle).toBeVisible();
+  });
+
+  test('Admin can delete a box', async () => {
+    await boxes.remove(admin, box);
+    cleanupQueue.box = false;
+  });
+
+  test('User cannot access deleted box', async () => {
+    await navigation.goToPhase(user, room.name, 10);
+    await expect(user.getByText(box.name)).toBeHidden();
+  });
+});
---
+++ tests/shared/interactions/boxes.ts
@@ -0,0 +1,65 @@
+import { expect, Page } from '@playwright/test';
+
+import * as types from '../../fixtures/types';
+import * as formInteractions from './forms';
+import * as navigation from './navigation';
+import * as settingsInteractions from './settings';
+
+export const create = async (page: Page, box: types.BoxData) => {
+  await navigation.goToBoxesSettings(page);
+
+  await page.waitForSelector('[data-testid="add-boxes-button"]', { state: 'visible', timeout: 500 });
+  await formInteractions.clickButton(page, 'add-boxes-button');
+  await page.waitForTimeout(500);
+
+  await sendForm(page, box);
+
+  await navigation.goToBoxesSettings(page);
+  await expect(page.getByTestId('add-boxes-button')).toBeVisible();
+  await settingsInteractions.check(page, { option: 'box_name', value: box.name });
+};
+
+export const edit = async (page: Page, box: types.BoxData) => {
+  await navigation.goToBoxesSettings(page);
+  await settingsInteractions.openEdit({ page, filters: { option: 'box_name', value: box.name } });
+  await sendForm(page, box);
+};
+
+export const fill = async (page: Page, box: types.BoxData) => {
+  await sendForm(page, box);
+};
+
+export const remove = async (page: Page, box: types.BoxData) => {
+  await navigation.goToBoxesSettings(page);
+
+  await settingsInteractions.remove({ page, scope: 'boxes', filters: { option: 'box_name', value: box.name } });
+};
+
+const sendForm = async (page: Page, box: types.BoxData) => {
+  await page.waitForSelector('[data-testid="box-name-input"]', { state: 'visible', timeout: 500 });
+
+  await formInteractions.fillForm(page, 'box-name-input', box.name);
+  await formInteractions.fillMarkdownForm(page, 'markdown-editor-description_public', box.description);
+
+  await formInteractions.selectOption(page, 'select-field-room_hash_id', box.room.name);
+
+  if (box.phase) {
+    await formInteractions.selectOptionByValue(page, 'select-field-phase_id', String(box.phase));
+  }
+
+  if (box.discussionDays) {
+    await formInteractions.fillForm(page, 'input-phase_duration_1', String(box.discussionDays));
+  }
+
+  if (box.votingDays) {
+    await formInteractions.fillForm(page, 'input-phase_duration_3', String(box.votingDays));
+  }
+
+  for (const i of box.ideas || []) {
+    await formInteractions.selectOption(page, 'ideas-autocomplete-field', i.name);
+  }
+
+  await formInteractions.clickButton(page, 'box-form-submit-button');
+  await page.waitForLoadState('networkidle');
+  await page.waitForTimeout(1000);
+};
