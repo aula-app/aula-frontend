72eeb7fcaac7b48fe7a501c6b1d7a0c4089dc72b
Eduardo Liron <eduardoliron@gmail.com>
2026-01-20 17:47:54 -0300

    add missing files

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      spacing: {
+        'safe-top': 'var(--safe-area-inset-top)',
+        'safe-right': 'var(--safe-area-inset-right)',
+        'safe-bottom': 'var(--safe-area-inset-bottom)',
+        'safe-left': 'var(--safe-area-inset-left)',
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
