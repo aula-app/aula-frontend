c7927e63b0fe4fa96973e1cf9f19ce562e04f2f4
Nikola Marić <206777381+nikola-maric-aula@users.noreply.github.com>
2026-02-04 12:49:35 +0100

    feat: add versions to bug reports header

--- src/services/requests-v2.ts
+++ src/services/requests-v2.ts
@@ -27,7 +27,11 @@ export const versionsRequest = async (): Promise<VersionsResponse> => {
   const instanceApiUrl = localStorageGet('api_url');
   const v1 = await baseVersionsRequest(`${instanceApiUrl}/api/controllers/versions.php`, 'v1');
   const v2 = await baseVersionsRequest(`${instanceApiUrl}/public/versions`, 'v2');
-  return { 'aula-backend.v1': v1['aula-backend'], 'aula-backend.v2': v2['aula-backend'] } as VersionsResponse;
+  return {
+    'aula-backend.v1': v1,
+    'aula-backend.v2': v2,
+    'aula-frontend': import.meta.env.VITE_APP_VERSION,
+  } as VersionsResponse;
 };
 
 const baseVersionsRequest = async (versionsUrl: string, version: string) => {
