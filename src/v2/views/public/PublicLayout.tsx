17989869f10089767bfdc3d3742d116c17fffe2c
Eduardo Liron <eduardoliron@gmail.com>
2026-05-26 08:55:37 -0300

    fix chip sizing acording to text size

--- src/v2/views/public/PublicLayout.tsx
+++ src/v2/views/public/PublicLayout.tsx
@@ -24,7 +24,7 @@ const PublicLayout: FunctionComponent<PropsWithChildren> = () => {
               </Link>
             )}
           </div>
-          <div>
+          <div className="text-sm">
             <CodeButton />
           </div>
         </nav>
