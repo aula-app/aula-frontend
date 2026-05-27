24a5094784c91e4ae068acc617940ca4267a96b3
Eduardo Liron <eduardoliron@gmail.com>
2026-05-27 15:25:15 -0300

    fix type error

--- src/v2/views/private/NotFound/NotFoundView.tsx
+++ src/v2/views/private/NotFound/NotFoundView.tsx
@@ -1,4 +1,5 @@
 import Icon from '@/components/new/Icon';
+import Button from '@/v2/components/button/Button/Button';
 import Tooltip from '@/v2/components/ui/Tooltip';
 import { useTranslation } from 'react-i18next';
 
@@ -9,8 +10,10 @@ const PublicNotFoundView = () => {
     <div className="flex-1 flex flex-col items-center justify-center gap-12" data-testid="not-found-view">
       <img src="/img/Paula_schlafend.svg" alt={t('v2.alt.sleeping')} loading="lazy" className="w-32" />
       <Tooltip content={t('v2.page.notFound.hint')} wrapperClassName="flex items-center gap-3 text-error" tapToShow>
-        <Icon type="alert" size="24" />
-        {t('v2.page.notFound.title')}
+        <Button text color="error" className="flex items-center gap-3">
+          <Icon type="alert" size="24" />
+          {t('v2.page.notFound.title')}
+        </Button>
       </Tooltip>
     </div>
   );
--- src/v2/views/public/Offline/OfflineView.tsx
+++ src/v2/views/public/Offline/OfflineView.tsx
@@ -1,4 +1,5 @@
 import Icon from '@/components/new/Icon';
+import Button from '@/v2/components/button/Button/Button';
 import Tooltip from '@/v2/components/ui/Tooltip';
 import { useTranslation } from 'react-i18next';
 
@@ -9,8 +10,10 @@ const OfflineView = () => {
     <div className="flex-1 flex flex-col items-center justify-around" data-testid="school-offline-view">
       <img src="/img/Paula_schlafend.svg" alt={t('v2.alt.sleeping')} loading="lazy" className="w-32" />
       <Tooltip content={t('v2.page.offline.hint')} wrapperClassName="flex items-center gap-3 text-error" tapToShow>
-        <Icon type="alert" size="24" />
-        {t('v2.page.offline.title')}
+        <Button text color="error" className="flex items-center gap-3">
+          <Icon type="alert" size="24" />
+          {t('v2.page.offline.title')}
+        </Button>
       </Tooltip>
     </div>
   );
