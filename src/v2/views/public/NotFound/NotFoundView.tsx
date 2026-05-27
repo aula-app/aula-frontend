8aafa8349844fb59fafcc6472a9a46b1306a97fd
Eduardo Liron <eduardoliron@gmail.com>
2026-05-27 15:20:49 -0300

    fix type error

--- src/v2/views/public/NotFound/NotFoundView.tsx
+++ src/v2/views/public/NotFound/NotFoundView.tsx
@@ -8,9 +8,11 @@ const PublicNotFoundView = () => {
   return (
     <div className="flex-1 flex flex-col items-center justify-around" data-testid="not-found-view">
       <img src="/img/Paula_schlafend.svg" alt={t('v2.alt.sleeping')} loading="lazy" className="w-32" />
-      <Tooltip content={t('v2.page.notFound.hint')} wrapperClassName="flex items-center gap-3 text-error" tapToShow>
-        <Icon type="alert" size="24" />
-        {t('v2.page.notFound.title')}
+      <Tooltip content={t('v2.page.notFound.hint')} wrapperClassName="text-error" tapToShow>
+        <span className="flex items-center gap-3">
+          <Icon type="alert" size="24" />
+          {t('v2.page.notFound.title')}
+        </span>
       </Tooltip>
     </div>
   );
