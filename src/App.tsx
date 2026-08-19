b75cd47a2eb26c47a786776c1916da79534eb1c2
Eduardo Liron <eduardoliron@gmail.com>
2026-08-19 14:54:37 -0300

    add intent filter to capacitor app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import { ErrorBoundary } from './components';
 import { getRuntimeConfig } from './config';
-import { useAndroidBackButton } from './hooks';
+import { useAndroidBackButton, useDeepLinks } from './hooks';
 import Layout from './layout';
 import Routes from './routes';
 import { AppStore } from './store';
@@ -14,6 +14,8 @@ import { AppThemeProvider } from './theme';
 const AppContent: React.FC = () => {
   // Handle Android back button navigation
   useAndroidBackButton();
+  // Route incoming App Links / Universal Links into the SPA
+  useDeepLinks();
 
   return (
     <Layout>
